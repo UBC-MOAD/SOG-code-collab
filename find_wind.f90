3b187bc50c2249b95d2d62d00285b3a2e805aa6e
Doug Latornell <djl@douglatornell.ca>
2006-07-16 13:25:34 +0000

    Corrected inequality operator from <> to /=.
    Changed "use declarations" to "use mean_param", and added wind_n as an
    argument.  This is a reversion to how the code was in kcollinsTC and
    dlatornell06 versions.  It resolves compilation errors that arise due
    to repeated declarations of year, day, and wind arguments.
    
    Did lots of code clean-up and applied emacs f90-indent-subprogram
    function.  Added CVS Id and Source keywords to top of file.

--- find_wind.f90
+++ find_wind.f90
@@ -1,91 +1,92 @@
-SUBROUTINE find_wind(year,day,time,ecmapp,unow,vnow,wind)
+! $Id$
+! $Source
 
-      USE declarations
-      USE surface_forcing
-
-      IMPLICIT NONE
-
-      INTEGER, INTENT(IN)::year,day
-      DOUBLE PRECISION, INTENT (IN):: time
-      INTEGER, INTENT(IN OUT)::ecmapp      
-      TYPE(wind_ecmwf), DIMENSION(wind_n), INTENT(IN)::wind
-      DOUBLE PRECISION, INTENT(OUT):: unow,vnow
+subroutine find_wind(year, day, time, ecmapp, wind_n, unow, vnow, wind)
+! Find the unow and vnow components of the wind at the specified 
+! year/day/time by interpolation in the wind data structure.
 
 
-      INTEGER :: deltat,requiredtime,done,wind_end
-      DOUBLE PRECISION :: withinhour,otime
+  use mean_param  ! *** Changed from declarations to resolve conflicts
+  use surface_forcing
 
-      count = ecmapp
-      deltat = wind(2)%time-wind(1)%time
+  implicit none
 
+  ! Arguments:
+  integer, intent(in) :: year, day, wind_n
+  double precision, intent(in) :: time
+  integer, intent(in out) :: ecmapp      
+  double precision, intent(out) :: unow, vnow
+  type(wind_ecmwf), dimension(wind_n), intent(in) :: wind
 
-      if (deltat <> 1) then
-           write (*,*) 'find wind oh no'
-           stop
-        endif
+  ! Local variables:
+  integer :: count, deltat, requiredtime, done
+  double precision :: withinhour, otime
 
-!      wind_end= 30802
+  count = ecmapp
+  deltat = wind(2)%time - wind(1)%time
+  if (deltat /= 1) then
+     write (*,*) '!!! Error: find_wind: deltat != 1'
+     stop
+  endif
 
-      otime = time - int(time/(365*86400))*365*86400
-      otime = otime - int(otime/86400)*86400
-      requiredtime = int(otime/3600)
-      withinhour = (otime-requiredtime*3600)/(deltat*3600)
-     
-      done = 0
+  otime = time - int(time / (365 * 86400)) *365 * 86400
+  otime = otime - int(otime / 86400) * 86400
+  requiredtime = int(otime / 3600)
+  withinhour = (otime - requiredtime * 3600) / (deltat * 3600)
 
-!print*,day,count,ecmapp,'fw1'
+  ! Set counter to frist day of year in wind data
+  ! *** This is not robust and needs to be fixed
+  if (day == 1 .AND. year == 2002) then
+     ecmapp = 8761
+     count = ecmapp
+  else if (day == 1 .AND. year == 2003) then
+     ecmapp = 17521
+     count = ecmapp
+  else if (day == 1 .AND. year == 2004) then
+     ecmapp = 26288
+     count = ecmapp
+  else if (day == 1 .AND. year == 2005) then
+     ecmapp = 35065
+     count = ecmapp
+  else if (day == 1 .AND. year == 2006) then
+     ecmapp = 43825
+     count = ecmapp
+  endif
 
-if (day==1 .AND. year==2002) then
-ecmapp=8761
-count=ecmapp
-else if (day==1 .AND. year==2003) then
-ecmapp=17521
-count=ecmapp
-else if (day==1 .AND. year==2004) then
-ecmapp=26281
-count=ecmapp
-else if (day==1 .AND. year==2005) then
-ecmapp=35065
-count=ecmapp
-else if (day==1 .AND. year==2006) then
-ecmapp=43825
-count=ecmapp
-endif
-
-      do while (done == 0)
-         if (wind(count)%Jday > day) then
-            count = count -1
-         else if (wind(count)%Jday < day) then
-            count = count + 1 
+  ! Search and interpolation loop
+  done = 0
+  do while (done == 0)
+     if (wind(count)%Jday > day) then
+        count = count - 1
+     else if (wind(count)%Jday < day) then
+        count = count + 1 
+     else
+        if (wind(count)%time > requiredtime) then
+           count = count - 1
+           write (*,*) '!!! Warning: find_wind: ', &
+                'time beyond end of wind data', count
+        else if (wind(count)%time < requiredtime) then
+           count = count + 1
+           write (*,*) '!!! Warning: find_wind: ', &
+                'time before start of wind data', count, day, wind(count)%Jday
         else
-            if (wind(count)%time > requiredtime) then
-               count = count - 1
-               write (*,*) 'time too far',count
-            else if (wind(count)%time < requiredtime) then
-               count = count + 1
-               write (*,*) 'time too small',count,day,wind(count)%Jday
-            else
-               vnow = wind(count)%meridional*(1-withinhour)+wind(count+1)%meridional*withinhour
-               unow = wind(count)%zonal*(1-withinhour)+wind(count+1)%zonal*withinhour
-!print*,'unow,vnow,wind(count)%meridional,wind(count)%zonal, otime,withinhour,requiredtime,time'
-!print*,unow,vnow,wind(count)%meridional,wind(count)%zonal,otime,withinhour,requiredtime,time
-!pause
+           ! Interpolation
+           vnow = wind(count)%meridional * (1 - withinhour) &
+                + wind(count+1)%meridional * withinhour
+           unow = wind(count)%zonal * (1 - withinhour) &
+                + wind(count + 1)%zonal * withinhour
+           done = 1
+           ecmapp = count
+           write (*,*) 'find_wind: time', count
+        end if
+     end if
 
-              done = 1
-               ecmapp = count
-               write (*,*) 'time', count
+     if (count < 0 .OR. count > wind_n) then
+        write (*,*) count, '!!! Error: find_wind: Interpolation failed'
+        write (*,*) count, year, day, time, wind(count)%year, &
+             wind(count)%Jday, wind(count)%time
+        stop
+     endif
+  end do
 
-            end if
-         end if
-         if (count < 0 .OR. count > wind_n) then
-
-            write (*,*) count, ' find wind'
-            write (*,*) count,year,day,time,wind(count)%year,wind(count)%Jday,wind(count)%time
-            stop
-         endif
-      end do
-
-    END SUBROUTINE find_wind
-
-
-
+end subroutine find_wind
