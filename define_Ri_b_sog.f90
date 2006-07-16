36602d451af6e9b4ea8e3f6ae8a931724f2c8597
Doug Latornell <djl@douglatornell.ca>
2006-07-16 14:00:37 +0000

    Added declarations of variables that were using implicit typing.
    Changed whitespace to improve readability of code.  Did some code
    clean-up.  Added CVS Id and Source keywords to top of file.

--- define_Ri_b_sog.f90
+++ define_Ri_b_sog.f90
@@ -1,43 +1,55 @@
-SUBROUTINE define_Ri_b_sog(d, hh, surf_h, Bb, Uu, Vv, dens, Rib, Vt_sq, N2)
+! $Id$
+! $Source$
+
+subroutine define_Ri_b_sog(d, hh, surf_h, Bb, Uu, Vv, dens, Rib, &
+     Vt_sq, N2)
+
+! Calculate the profile of the bulk Richardson number, which is
+! (probably) used to find the bottom of the mixed layer
 
   USE mean_param
   USE surface_forcing
 
-  TYPE(gr_d), INTENT(IN)::d
-  TYPE(height), INTENT(IN)::hh 
-  TYPE(height), INTENT(OUT)::surf_h
-  TYPE(prop), INTENT(IN OUT)::Bb, Uu, Vv, dens
-  DOUBLE PRECISION, DIMENSION(d%M), INTENT(IN)::Vt_sq
-  DOUBLE PRECISION, DIMENSION(d%M), INTENT(OUT)::Rib
-  DOUBLE PRECISION, DIMENSION(0:d%M+1), INTENT(IN)::N2
+  ! Arguments:
+  type(gr_d), intent(in) :: d
+  type(height), intent(in) :: hh 
+  type(height), intent(out) :: surf_h
+  type(prop), intent(in out) :: Bb, Uu, Vv, dens
+  double precision, dimension(d%M), intent(in) :: Vt_sq
+  double precision, dimension(d%M), intent(out) :: Rib
+  double precision, dimension(0:d%M+1), intent(in) :: N2
  
-  DOUBLE PRECISION, DIMENSION(0:d%M+1)::test_vector, test_vector2
-  INTEGER::yy
+  ! Local variables:
+  double precision, dimension(0:d%M+1) :: test_vector, test_vector2
+  integer :: yy
+  double precision ::Ribmin
 
   surf_h%new = ep*hh%new
        
-  CALL average(d,Uu,surf_h) !test conv  
-  CALL average(d,Vv,surf_h) !test conv 
-  CALL average(d,dens,surf_h)
+  call average(d,Uu,surf_h) !test conv  
+  call average(d,Vv,surf_h) !test conv 
+  call average(d,dens,surf_h)
 
-  test_vector = (Uu%avg-Uu%new)**2.0+(Vv%avg-Vv%new)**2.0 
-  test_vector2 =  -g/dens%new(0)*(dens%avg - dens%new) !Bb%avg - Bb%new  
+  test_vector = (Uu%avg - Uu%new)**2.0 + (Vv%avg - Vv%new)**2.0 
+  test_vector2 =  -g / dens%new(0) * (dens%avg - dens%new) !Bb%avg - Bb%new  
          
   Rib = 0.0
   Ribmin = 1000.
            
-  DO yy = 1, d%M !surf_h%g, d%M 
-     IF (N2(yy) >= 0.) THEN
-        IF (test_vector2(yy) > EPSILON(test_vector2(yy)) .OR. &
-             test_vector2(yy) < -EPSILON(test_vector2(yy))) THEN
-           Rib(yy) = test_vector2(yy)*d%d_g(yy)/&
-                (test_vector(yy)+Vt_sq(yy) + 1.0D-30)
-           if (Rib(yy).lt.Ribmin) then
+  do yy = 1, d%M !surf_h%g, d%M 
+     if (N2(yy) >= 0.) then
+        if (test_vector2(yy) > EPSILON(test_vector2(yy)) .OR. &
+             test_vector2(yy) < -EPSILON(test_vector2(yy))) then
+           Rib(yy) = test_vector2(yy) * d%d_g(yy) &
+                / (test_vector(yy) + Vt_sq(yy) + 1.0D-30)
+           if (Rib(yy) < Ribmin) then
                Ribmin = Rib(yy)
+               ! *** This line doesn't appear to have any purpose,
+               ! *** ymin is never used
                ymin = yy
             endif
-        END IF
-     END IF
-  END DO
+        end if
+     end if
+  end do
   
-END SUBROUTINE define_Ri_b_sog
+end subroutine define_Ri_b_sog
