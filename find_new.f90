275ac186d93bbe4ccae5fe0a3dca557c36e60488
Doug Latornell <djl@douglatornell.ca>
2006-07-17 09:05:01 +0000

    Deleted EXTERNAL and type declarations to the following: erfc, node_wt, rtbis, rtnewt, wt_dwt, zbrac, zbrak.  This resolves and undefined symbol problem in linking with g95, and will work fine with pgf90 too.
    Added some documentation , and some comments on future refinements.
    Added CVS ID and Source keywords to top of file.

--- find_new.f90
+++ find_new.f90
@@ -1,28 +1,31 @@
-SUBROUTINE find_new
+! $Id$
+! $Source$
 
-  USE surface_forcing
-  USE declarations
-!  USE Copepod_mod
+subroutine find_new
+  ! *** What's it do?
 
-  IMPLICIT NONE
+  use surface_forcing
+  use declarations
 
-  EXTERNAL erfc, gammq, node_wt, rtbis, zbrac, zbrak, rtnewt, wt_dwt
-!  INTEGER,INTENT(IN)::max_length
+  implicit none
+
+  ! *** These are not used here
+  external gammq, rtnewt
+  real :: gammq
+  double precision :: rtnewt
+
+  ! Local variables:
+  ! *** though lots of them are unused
   DOUBLE PRECISION, DIMENSION(max_length)::wt_bin_size  !MAXVAL(Cevent%length)
   DOUBLE PRECISION::last_dwt
   REAL::t2
-  DOUBLE PRECISION::node_wt_max,node_wt_min,accuracy, rtnewt
-  DOUBLE PRECISION::node_wt,rtbis,node_wt_old,node_guess
+  DOUBLE PRECISION::node_wt_max,node_wt_min,accuracy
+  DOUBLE PRECISION::node_wt_old,node_guess
   DOUBLE PRECISION::fun,dfun
-  REAL::erfc,gammq
   LOGICAL::succes
   INTEGER::nb,stepb
+
   wt_bin_size = 1.0
-
-
-  !test function node_wt
-
-
   DO yy = 1, M
 
      IF (P1_p(yy) < zero) THEN
@@ -48,21 +51,20 @@ SUBROUTINE find_new
      P%micro%new(yy) = P1_p(yy)
      N%O%new(yy) = NO1_p(yy)
      N%H%new(yy) = NH1_p(yy)
-     
+
   END DO
-  
+
   P%micro%new(0) = P%micro%new(1)
   N%O%new(0) = N%O%new(1)
   N%H%new(0) = N%H%new(1)
-  
+
   P%micro%new(M+1) = P%micro%old(M+1)
   N%O%new(M+1) = N%O%old(M+1)
   N%H%new(M+1) = N%H%old(M+1)
- 
- DO xx = 1,D_bins
+
+  do xx = 1, D_bins
      Detritus(xx)%D%new(0) = Detritus(xx)%D%new(1)
      Detritus(xx)%D%new(M+1) = Detritus(xx)%D%old(M+1)
- END DO
-  
-   
-END SUBROUTINE find_new
+  end do
+
+end subroutine find_new
