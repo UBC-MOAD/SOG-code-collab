fe0c5e0b1e2248763cb17bd284fb6f80ec873873
Doug Latornell <djl@douglatornell.ca>
2006-08-08 15:42:34 +0000

    Manually merged Nilgun's FLAGELLA changes by using diff between FLAGELLA and kcollins_NH4 versions as a guide.

--- derivs_sog.f90
+++ derivs_sog.f90
@@ -38,21 +38,32 @@ subroutine derivs_sog(x_var, nvar1, Y_var, DYDX_deriv, Temp)
 
   N%O_uptake%new = 0.
   N%H_uptake%new = 0.
+  waste%small = 0.  ! V.flagella.01 rel. to nanoplankton
   waste%medium = 0.
   N%remin = 0.
 
   CALL p_growth(Yplus,Yplus(1:M),nvar1,I_par,grid,N,micro,Temp,Resp) !microplankton
-  !      waste%medium = waste%medium + micro%M_z*Yplus(1:M)
-  waste%medium = waste%medium + Resp(M+1:2*M)*Yplus(1:M)
-
+!      waste%medium = waste%medium + micro%M_z*Yplus(1:M) !comm.add V.flagella.01->similar line
+!org.line      waste%medium = waste%medium + Resp(M+1:2*M)*Yplus(1:M)
+  waste%medium = waste%medium + Resp(1:M)*Yplus(1:M)
+  
+  CALL p_growth(Yplus,Yplus(M+1:2*M),nvar1,I_par,grid,N,nano,Temp,Resp) !nanoplankton !add V.flagella.01
+  ! maybe exchange the belo line with above. check the M number. 
+  !CALL p_growth(Yplus,Yplus(1:M),nvar1,I_par,grid,N,nano,Temp,Resp) !nanoplankton !add V.flagella.01
+  waste%small = waste%small + Resp(M+1:2*M)*Yplus(M+1:2*M) !waste%small is rel. to nano !add V.flagella.01
+  ! V.flagella.01 the line below is noted as the nano natural mortality in KPP
+  !      waste%small = waste%small + nano%M_z*Yplus(2*M+1:3:M) !waste%small is rel. to nano !add V.flagella.01
+  
   DYDX_deriv(1:nvar1) = 0.      
 
 !!!New quantity, bacterial 0xidation of NH to NO pool ==> NH^2
-  N%bacteria(1:M) = N%r*Yplus(2*M+1:3*M)**2.0
+  !V.flagella.org      N%bacteria(1:M) = N%r*Yplus(2*M+1:3*M)**2.0
+  N%bacteria(1:M) = N%r*Yplus(3*M+1:4*M)**2.0  ! mod. in V.flagella.01
 
 
   DO k_k = 1,D_bins-1
-     N%remin(:) = N%remin(:) + Detritus(k_k)%r*Yplus(3*M+(k_k-1)*M+1:3*M+k_k*M)
+     N%remin(:) = N%remin(:) + Detritus(k_k)%r*Yplus(4*M+(k_k-1)*M+1:4*M+k_k*M) !V.flagella.01
+     !V.flagella.org N%remin(:) = N%remin(:) + Detritus(k_k)%r*Yplus(3*M+(k_k-1)*M+1:3*M+k_k*M)
   END DO
   IF (MINVAL(N%remin) < 0.) THEN
      PRINT "(A)","N%remin < 0. in derivs.f90"
@@ -78,18 +89,19 @@ subroutine derivs_sog(x_var, nvar1, Y_var, DYDX_deriv, Temp)
              - Resp(j_j+M)) * Yplus(j_j)
         ! DYDX_deriv(j_j) = (micro%growth%new(j_j)-Resp(j_j)-micro%M_z)*Yplus(j_j)
      else if (j_j > M .AND. j_j <= 2*M .AND. Yplus(j_j) > 0.) then  
-        ! NO
-        DYDX_deriv(j_j) = -N%O_uptake%new(j_j-M) + N%bacteria(j_j-M)
-     else if (j_j > 2*M .AND. j_j <= 3*M) then  
-        ! NH
-        DYDX_deriv(j_j) = -N%H_uptake%new(j_j-2*M) + Resp(j_j-2*M) &
-             + waste%medium(j_j-2*M) * waste%m%destiny(0)          &
-             + N%remin(j_j-2*M) - N%bacteria(j_j-2*M) 
-     else if (j_j > 3*M .AND. j_j <= 3*M+D_bins*M) then
-        ! Detritus
+        DYDX_deriv(j_j) = (nano%growth%new(j_j-M)-Resp(j_j-M)-Resp(j_j))*Yplus(j_j-M) !V.flagella.01 not sure about this line
+        !V.flagella.01 the lines below are mod -> (multiplier+1)*M
+     ELSE IF (j_j > 2*M .AND. j_j <= 3*M .AND. Yplus(j_j) > 0.) THEN  
+        !NO
+        DYDX_deriv(j_j) = -N%O_uptake%new(j_j-2*M) +N%bacteria(j_j-2*M)
+     ELSE IF (j_j > 3*M .AND. j_j <= 4*M) THEN  
+        !NH
+        DYDX_deriv(j_j) = -N%H_uptake%new(j_j-3*M) + Resp(j_j-3*M) + waste%medium(j_j-3*M)*waste%m%destiny(0)+N%remin(j_j-3*M) - N%bacteria(j_j-3*M) 
+     ELSE IF (j_j > 4*M .AND. j_j <= 4*M+D_bins*M) THEN 
+        !Detritus
         do k_k = 1, D_bins-1
-           if (j_j > 3 * M + (k_k-1) * M .AND. j_j <= 3 * M + k_k * M) then
-              DYDX_deriv(j_j) = waste%medium(j_j-(3*M+(k_k-1)*M)) &
+           if (j_j > 4 * M + (k_k-1) * M .AND. j_j <= 4 * M + k_k * M) then
+              DYDX_deriv(j_j) = waste%medium(j_j - (4 * M + (k_k - 1) * M)) &
                    * waste%m%destiny(k_k) - Detritus(k_k)%r * Yplus(j_j) 
            end if
         end do
@@ -97,7 +109,8 @@ subroutine derivs_sog(x_var, nvar1, Y_var, DYDX_deriv, Temp)
   end do
 
 
-  PO_deriv(1:M) = DYDX_deriv(1:M)
+  PO_deriv(1:M) = DYDX_deriv(1:M) + DYDX_deriv(M+1:2*M) !V.flagella.01 the total Phyto+Zoo?
+  !V.flagella.org PO_deriv(1:M) = DYDX_deriv(1:M)
 
   ! Integrate rate of urea production
   ! *** More hard-coded constants to get rid of
@@ -105,7 +118,8 @@ subroutine derivs_sog(x_var, nvar1, Y_var, DYDX_deriv, Temp)
   ! *** trouble with index out of bounds runtime errors in sum_g()
   ! In the top 5 meters
   bottom = 5.  
-  call sum_g(grid, DYDX_deriv(M+1:2*M), bottom, NO50_rate)
+  call sum_g(grid, DYDX_deriv(2*M+1:3*M), bottom, NO50_rate)  !V.flagella.01
+  !V.flagella.org CALL sum_g(grid,DYDX_deriv(M+1:2*M),bottom2,NO50_rate)
   call sum_g(grid, PO_deriv(1:M), bottom, PO50_rate)
   ! In the whole model domain
   bottom = 40.
