bd6edea6328c42a1112b3812c43b9f5a5f5f2b41
Susan Allen <sallen@eos.ubc.ca>
2006-08-10 15:03:09 +0000

    As only the length of the grid is used, send in just M not the whole grid variable.
    Send in PZ_bins rather than the length M2
    
    As PZ is sent in, it is split into quantities (mico, nitrate etc) and detritus.
    Make this more obvious by calling first part PZ_quant
    
    Change the array size on PZ_quant to reflect that it is only the quantities
    not the whole array.  NOTE this is a substantive change but had no effect
    on the results when running on coho.
    
    Use PZ%bins rather than hard coding the position of quantities.
    
    White space clean up

--- reaction_p_sog.f90
+++ reaction_p_sog.f90
@@ -1,25 +1,40 @@
-SUBROUTINE reaction_p_sog(mm,nvar,D_bins,PZ,PZ_det,Pmicro,NO,NH,Detritus,Gvector)
+SUBROUTINE reaction_p_sog(M, PZ_bins, D_bins, PZ_quant, PZ_det, Pmicro, NO, &
+     NH, Detritus, Gvector)
 
       USE mean_param
 
       IMPLICIT NONE
 
-      TYPE(gr_d), INTENT(IN)::mm
-      INTEGER, INTENT(IN)::nvar, D_bins  !
-      DOUBLE PRECISION, DIMENSION(nvar), INTENT(IN)::PZ
-      DOUBLE PRECISION, DIMENSION(D_bins*mm%M),INTENT(IN)::PZ_det
-      DOUBLE PRECISION, DIMENSION(0:mm%M+1), INTENT(IN)::Pmicro,NO, NH
+      INTEGER, INTENT(IN)::M
+      TYPE (bins), INTENT(IN) :: PZ_bins
+      INTEGER, INTENT(IN)::D_bins  !
+      DOUBLE PRECISION, DIMENSION(PZ_bins%Quant*M), INTENT(IN)::PZ_quant
+      DOUBLE PRECISION, DIMENSION(D_bins*M),INTENT(IN)::PZ_det
+      DOUBLE PRECISION, DIMENSION(0:M+1), INTENT(IN)::Pmicro, NO, NH
       TYPE(snow), DIMENSION(D_bins), INTENT(IN)::Detritus
       TYPE(UVST), INTENT(OUT)::Gvector
-
+! Local variables
+      INTEGER :: bPZ, ePZ ! start position and end position in PZ array
       INTEGER::j
 
-      Gvector%p%micro = PZ(1:mm%M) - Pmicro(1:mm%M)
-      Gvector%N%O = PZ(mm%M+1:2*mm%M) - NO(1:mm%M)
-      Gvector%N%H = PZ(2*mm%M+1:3*mm%M) - NH(1:mm%M)
+      ! Micro plankton
+      bPZ = (PZ_bins%micro-1) * M + 1
+      ePZ = PZ_bins%micro * M
+      Gvector%p%micro = PZ_quant(bPZ:ePZ) - Pmicro(1:M)
 
+      ! Nitrate
+      bPZ = (PZ_bins%NO-1) * M + 1
+      ePZ = PZ_bins%NO * M
+      Gvector%N%O = PZ_quant(bPZ:ePZ) - NO(1:M)
+
+      ! Ammonimum
+      bPZ = (PZ_bins%NH-1) * M + 1
+      ePZ = PZ_bins%NH * M
+      Gvector%N%H = PZ_quant(bPZ:ePZ) - NH(1:M)
+
+      ! Detritus
       DO j = 1,D_bins
-         Gvector%d(j)%bin = PZ_det((j-1)*mm%M+1:j*mm%M) - Detritus(j)%D%old(1:mm%M)
+         Gvector%d(j)%bin = PZ_det((j-1)*M+1:j*M) - Detritus(j)%D%old(1:M)
       END DO
 
 END SUBROUTINE reaction_p_sog
