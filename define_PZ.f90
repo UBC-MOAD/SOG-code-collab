8dfe749c4d2a5ace376c7dd269c0534031aae42d
Nilgun Cetin <ncetin@eos.ubc.ca>
2006-08-18 05:46:28 +0000

    The order of PZ array is rearranged to include nanoplanktons in position number 2. It matches now to the derivs_sog (I need to check it once more to make sure).and SOG.
    After this first commit mixing becomes too shallow. Run message: "minimum mixing  0.25".

--- define_PZ.f90
+++ define_PZ.f90
@@ -20,17 +20,37 @@ SUBROUTINE define_PZ(check)
      IF (xx <= M) THEN
         PZ(xx) = P%micro%new(xx)
      ELSE IF (xx > M .AND. xx <= 2*M) THEN
-        PZ(xx) = N%O%new(xx-M)
+        PZ(xx) = P%nano%new(xx-M)
      ELSE IF (xx > 2*M .AND. xx <= 3*M) THEN
-        PZ(xx) = N%H%new(xx-2*M)
-     ELSE IF (xx > 3*M .AND. xx <= 3*M+D_bins*M) THEN 
+        PZ(xx) = N%O%new(xx-2*M)
+     ELSE IF (xx > 3*M .AND. xx <= 4*M) THEN
+        PZ(xx) = N%H%new(xx-3*M)
+     ELSE IF (xx > 4*M .AND. xx <= 4*M+D_bins*M) THEN 
         DO yy = 1,D_bins    ! 1 is suspended particulate N (SUS), 2 is sinking PN (SPN), and 3 is fast sink
-           IF (xx > 3*M+(yy-1)*M .AND. xx <= 3*M+yy*M) THEN
-             PZ(xx) = Detritus(yy)%D%new(xx-(3*M+(yy-1)*M))
+           IF (xx > 4*M+(yy-1)*M .AND. xx <= 4*M+yy*M) THEN
+             PZ(xx) = Detritus(yy)%D%new(xx-(4*M+(yy-1)*M))
            END IF
         END DO
      END IF
   END DO
+
+! Nanoplanktons are placed after microplanktons. 
+! The whole scheme is different now. See above
+!  DO xx = 1, M2
+!     IF (xx <= M) THEN
+!        PZ(xx) = P%micro%new(xx)
+!     ELSE IF (xx > M .AND. xx <= 2*M) THEN
+!        PZ(xx) = N%O%new(xx-M)
+!     ELSE IF (xx > 2*M .AND. xx <= 3*M) THEN
+!        PZ(xx) = N%H%new(xx-2*M)
+!     ELSE IF (xx > 3*M .AND. xx <= 3*M+D_bins*M) THEN 
+!        DO yy = 1,D_bins    ! 1 is suspended particulate N (SUS), 2 is sinking PN (SPN), and 3 is fast sink
+!           IF (xx > 3*M+(yy-1)*M .AND. xx <= 3*M+yy*M) THEN
+!             PZ(xx) = Detritus(yy)%D%new(xx-(3*M+(yy-1)*M))
+!           END IF
+!        END DO
+!     END IF
+!  END DO
 
 !      open(558,file="output/PZ.dat")!
 !      do isusan=1,M2
