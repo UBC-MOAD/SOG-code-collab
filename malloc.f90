599c75c13392b7aa2fb802427f5477c021207d48
Doug Latornell <djl@douglatornell.ca>
2006-08-20 09:49:25 +0000

    New module.
    Provides alloc_check and dalloc_check subroutines for other modules to
    use to check allocation/deallocation status, and print an error
    message in the case of failure.
    
    May be used in future as the place for allocation of memory for items
    that don't logically fit in any other module (but only as a last
    resort).

---
+++ malloc.f90
@@ -0,0 +1,62 @@
+! $Id$
+! $Source$
+
+module malloc
+  ! Memory allocation module.  Supplied memory allocation and
+  ! deallocation status check subroutines.
+  ! *** May also be the place for allocation subroutines for
+  ! *** quantities that can't logically be allocated anywhere else.
+  !
+  ! Public Functions:
+  !
+  ! alloc_check(stat, msg) -- Check the stat result of an allocate
+  !                           statement.  If it is non-zero, output 
+  !                           the specified message on stderr and stop 
+  !                           execution.
+  !
+  ! dalloc_check(stat, msg) -- Check the stat result of an deallocate
+  !                            statement.  If it is non-zero, output 
+  !                            the specified message on stderr and stop 
+  !                            execution.
+
+  implicit none
+
+  private
+  public :: alloc_check, dalloc_check
+
+contains
+
+  subroutine alloc_check(alloc_stat, message)
+    ! Check the stat result of an allocate statement.
+    ! If it is non-zero, output the specified message on stderr
+    ! and stop execution.
+    use io_unit_defs
+    implicit none
+    ! Arguments:
+    integer, intent(in)           :: alloc_stat
+    character(len=80), intent(in) :: message
+
+    if (alloc_stat /= 0) then
+       write(stderr, *) trim(message) // " memory allocation failed."
+       stop
+    end if
+  end subroutine alloc_check
+
+
+  subroutine dalloc_check(dalloc_stat, message)
+    ! Check the stat result of an deallocate statement.
+    ! If it is non-zero, output the specified message on stderr
+    ! and stop execution.
+    use io_unit_defs
+    implicit none
+    ! Arguments:
+    integer, intent(in)           :: dalloc_stat
+    character(len=80), intent(in) :: message
+
+    if (dalloc_stat /= 0) then
+       write(stderr, *) trim(message) // " memory deallocation failed."
+       stop
+    end if
+  end subroutine dalloc_check
+
+end module malloc
