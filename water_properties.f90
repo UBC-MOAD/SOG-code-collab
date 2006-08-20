e76a91a16880a4b8e66ffd884ad7159d385caaba
Doug Latornell <djl@douglatornell.ca>
2006-08-20 10:03:13 +0000

    New module that results from refactoring of Cp_sub.   alpha_sub, coefficients, and density_sub will also be refactored into this module.
    alloc_water_props subroutine allocates memory for Cp, other properties
    to follow.
    
    dalloc_water_props subroutine deallocates memory for Cp, other
    properties to follow.
    
    Cp_profile function calculates Cp at grid point depths; The formula
    was changed to use same equation as Rich Pawlowicz's swcp.m Matlab
    function that is based on Millero, et al; UNESCO, 1981.
    
    The change in Cp formulation results in minor changes to the profiles
    and time series output so that they no longer diff cleanly against the
    previous reference copies.  The differences are not significant on
    profile plots, although minor differences remain in the profiles
    generated by runs on coho and on iBooks.

---
+++ water_properties.f90
@@ -0,0 +1,121 @@
+! $Id$
+! $Source$
+
+module water_properties
+  ! Type definition, constants, functions, and subroutines to
+  ! calculate water properties: specific heat capacity (Cp), thermal
+  ! expansion coefficient (alpha), salinity expansion coefficient
+  ! (beta), and density.
+  !
+  ! Public Type:
+  ! 
+  ! water_property -- Thermal and salinity expansion coefficients,
+  !                   heat capacity, and density profiles.
+  ! 
+  ! Public Functions:
+  !
+  ! Cp_profile(T, S) -- Return the heat capacity of the water column at
+  !                     the grid point depths based on the supplied water 
+  !                     temperature and salinity profiles.
+  ! 
+  ! Public Subroutines:
+  !
+  ! alloc_water_props() -- Allocate memory for water property profiles.
+  !
+  ! dalloc_water_props() -- Deallocate memory from water property
+  !                         profiles.
+
+  use precision_defs
+
+  implicit none
+
+  private
+  public :: &
+       ! Types:
+       water_property, &
+       ! Functions:
+       Cp_profile, &
+       ! Subroutines:
+       alloc_water_props, &
+       dalloc_water_props
+
+  ! Public type definition:
+  !
+  ! Thermal and salinity expansion coefficients, heat capacity, and
+  ! density profiles
+  type :: water_property
+     real(kind=dp), dimension(:), pointer :: &
+          g,    &  ! Value at grid point depth
+          gdiv, &  ! Rate of change (d/dz) at grid point depth
+          i,    &  ! Value at grid cell interface depth
+          idiv     ! Rate of change (d/dz) at grid cell interface depth
+     ! Rate of change values are used only for expansion coefficients
+  end type water_property
+
+contains
+
+  subroutine alloc_water_props(M, Cp)
+    ! Allocate memory for water property profiles.
+    use malloc
+    implicit none
+    ! Argument:
+    integer, intent(in)               :: M   ! Number of grid points
+    type(water_property), intent(out) :: Cp  ! Heat capacity profile [J/kg.K]
+    ! Local variables:
+    integer              :: allocstat  ! Allocation return status
+    character(len=80)    :: msg        ! Allocation failure message prefix
+
+    msg = "Water heat capacity profile (Cp) arrays"
+    allocate(Cp%g(0:M+1), Cp%i(0:M), stat=allocstat)
+    call alloc_check(allocstat, msg)
+  end subroutine alloc_water_props
+
+
+  subroutine dalloc_water_props(Cp)
+    ! Deallocate memory for water property profiles.
+    use malloc
+    implicit none
+    ! Argument:
+    type(water_property), intent(out) :: Cp  ! Heat capacity profile [J/kg.K]
+    ! Local variables:
+    integer              :: dallocstat  ! Allocation return status
+    character(len=80)    :: msg         ! Allocation failure message prefix
+
+    msg = "Water heat capacity profile (Cp) arrays"
+    deallocate(Cp%g, Cp%i, stat=dallocstat)
+    call dalloc_check(dallocstat, msg)
+  end subroutine dalloc_water_props
+
+
+  function Cp_profile(T, S) result(Cp_g)
+    ! Return the specific heat capacity of the water column in J/kg-K
+    ! at the grid point depths based on the supplied water temperature
+    ! and salinity profiles.
+    !
+    ! Based on Rich Pawlowicz's swcp.m Matlab function in his OCEANS
+    ! toolbox (http://www.eos.ubc.ca/~rich/#OCEANS) with the pressure
+    ! dependency removed.
+    !
+    ! Check value: Cp(10., 24.) = 4047.5 J/kg-K
+    use unit_conversions
+    implicit none
+    ! Arguments:
+    real(kind=dp), dimension(0:), intent(in) :: T    ! Temperature [K]
+    real(kind=dp), dimension(0:), intent(in) :: S    ! Salinity [-]
+    ! Result:
+    real(kind=dp), dimension(0:size(T) - 1) :: Cp_g  ! Specific heat capacity
+    ! Local variables:
+    real(kind=dp), dimension(0:size(T) - 1) :: TC, A, B, C
+
+    ! Convert the temperature to Celsius
+    TC = KtoC(T)
+    ! Calculate the heat capacity profile at the grid point depths
+    ! Specific heat Cp0 for P=0 (Millero, et al; UNESCO 1981)
+    A = (-1.38385E-3 * TC + 0.1072763) * TC - 7.643575
+    B = (5.148E-5 * TC - 4.07718E-3) * TC + 0.1770383
+    C = (((2.093236E-5 * TC - 2.654387E-3) * TC + 0.1412855) &
+         * TC - 3.720283) * TC + 4217.4
+    Cp_g = (A + B * sqrt(S)) * S + C
+  end function Cp_profile
+
+end module water_properties
