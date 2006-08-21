078dee23c860ff06445370870f76d6dca9240d73
Nilgun Cetin <ncetin@eos.ubc.ca>
2006-08-21 17:19:08 +0000

    Changed the write output format back to 4 decimal places.

--- write_physical_sog.f90
+++ write_physical_sog.f90
@@ -47,8 +47,8 @@ subroutine write_physical_sog(unow, vnow, euphotic)
      endif
   enddo
 
-111 format (17(1x,f15.9)) !V.flagella.01  
-! V.flagella.0 111 format (17(1x,f10.4))   
+111 format (17(1x,f10.4))   
+
   write(293, 111) time/3600., h%new, stress%u%new, stress%v%new,t%new(0), &
        s%new(0), u%new(1), Q_t(0), K%t%all(1), I_par(0), density%new(0), &
        Q_n(1)*100000.
