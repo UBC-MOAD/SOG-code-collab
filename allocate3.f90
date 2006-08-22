be0ea279498ca4cef675b776eba31fc37e1e44ba
Doug Latornell <djl@douglatornell.ca>
2006-08-22 08:28:08 +0000

    Moved allocation of PZ in from deprecated allocate4.f90.
    Refactored to use alloc_check from malloc module, and only clause on
    declarations module.  Removed use of mean_param and surface_forcing.
    Added documentation.

--- allocate3.f90
+++ allocate3.f90
@@ -1,27 +1,39 @@
-SUBROUTINE allocate3(STAT)
+! $Id$
+! $Source$
 
-      USE mean_param
-      USE surface_forcing
-      USE declarations
-!      USE Copepod_mod
+subroutine allocate3
+  ! Allocate memory for biological model arrays (at least some of them...)
 
-      IMPLICIT NONE
-          
-      INTEGER, DIMENSION(20), INTENT(IN OUT)::STAT
+  use malloc, only: alloc_check
+  use declarations, only: M, M2, D_bins, PZ, Gvector, Gvector_o, &
+       Gvector_o_o, Gvector_ao, Gvector_ao_o, Gvector_ro,        &
+       Gvector_ro_o, Hvector, Detritus
 
-      INTEGER::y_y, x_x
+  implicit none
 
+  ! Local variables:
+  integer              :: allocstat  ! Allocation return status
+  character(len=80)    :: msg        ! Allocation failure message prefix
+  integer              :: y_y        ! Loop index
 
-     ALLOCATE(cgraze(prey+d_prey,M))
+  msg = "PZ vector (biology model timestep initial conditions) array"
+  allocate(PZ(M2), stat=allocstat)
+  call alloc_check(allocstat, msg)
+
+  do y_y = 1, D_bins
+     msg = "Biology model Gvector bin components arrays"
+     allocate(Gvector%d(y_y)%bin(M), Gvector_o%d(y_y)%bin(M),     &
+          Gvector_o_o%d(y_y)%bin(M), Gvector_ao%d(y_y)%bin(M),    &
+          Gvector_ao_o%d(y_y)%bin(M), Gvector_ro%d(y_y)%bin(M),   &
+          Gvector_ro_o%d(y_y)%bin(M), stat=allocstat)
+     call alloc_check(allocstat, msg)
+     msg = "Biology model Hvector bin component array"
+     allocate(Hvector%d(y_y)%bin(M), stat=allocstat)
+     call alloc_check(allocstat, msg)
+     msg = "Detritus component arrays"
+     allocate(Detritus(y_y)%D%new(0:M+1), Detritus(y_y)%D%old(0:M+1), &
+          Detritus(y_y)%D%old_old(0:M+1), stat=allocstat)
+     call alloc_check(allocstat, msg)
+  end do
       
-      DO y_y = 1,D_bins
-         ALLOCATE(Gvector%d(y_y)%bin(M),Gvector_o%d(y_y)%bin(M),Gvector_o_o%d(y_y)%bin(M),&
-              Gvector_ao%d(y_y)%bin(M),Gvector_ao_o%d(y_y)%bin(M),Gvector_ro%d(y_y)%bin(M),&
-              Gvector_ro_o%d(y_y)%bin(M),Hvector%d(y_y)%bin(M),&
-              Detritus(y_y)%D%new(0:M+1),Detritus(y_y)%D%old(0:M+1),Detritus(y_y)%D%old_old(0:M+1))
-      END DO
-
-      
-END SUBROUTINE allocate3
-
-
+end subroutine allocate3
