7d9d7152484103dee60a24e8815674d380fd768b
Susan Allen <sallen@eos.ubc.ca>
2006-08-31 08:58:54 +0000

    Added explanation as to what this subroutine does. Put only blocks on the use statements Removed unused local variables Added Silicon Removed bottom boundary conditions as they are reset directly afterward in SOG.

--- find_new.f90
+++ find_new.f90
@@ -2,29 +2,18 @@
 ! $Source$
 
 subroutine find_new
-  ! *** What's it do?
+  ! Copies results from tridiagonal solver into real variable names,
+  !removing any negative values.  Applies upper boundary conditions
 
-  use surface_forcing
-  use declarations
+  use surface_forcing, only: zero
+  use declarations, only: P1_p, Pnano1_p, NO1_p, NH1_p, SIL1_p, Detritus1_p,&
+       P, N, Sil, Detritus, D_bins, neg_count, M
 
   implicit none
 
-  ! *** This is not used here
-  external gammq
-  real :: gammq
-
   ! Local variables:
-  ! *** though lots of them are unused
-  DOUBLE PRECISION, DIMENSION(max_length)::wt_bin_size  !MAXVAL(Cevent%length)
-  DOUBLE PRECISION::last_dwt
-  REAL::t2
-  DOUBLE PRECISION::node_wt_max,node_wt_min,accuracy
-  DOUBLE PRECISION::node_wt_old,node_guess
-  DOUBLE PRECISION::fun,dfun
-  LOGICAL::succes
-  INTEGER::nb,stepb
+  INTEGER:: xx, yy
 
-  wt_bin_size = 1.0
   DO yy = 1, M
 
      IF (P1_p(yy) < zero) THEN
@@ -43,6 +32,11 @@ subroutine find_new
         NH1_p(yy) = zero
         neg_count = neg_count + 1
      END IF
+     IF (SIL1_p(yy) < zero) THEN
+        SIL1_p(yy) = zero
+        neg_count = neg_count + 1
+     END IF
+
      DO xx = 1,D_bins
         IF (Detritus1_p(xx,yy) < zero) THEN
            Detritus1_p(xx,yy) = zero
@@ -55,6 +49,7 @@ subroutine find_new
      P%nano%new(yy) = Pnano1_p(yy)
      N%O%new(yy) = NO1_p(yy)
      N%H%new(yy) = NH1_p(yy)
+     Sil%new(yy) = SIL1_p(yy)
 
   END DO
 
@@ -62,15 +57,10 @@ subroutine find_new
   P%nano%new(0) = P%nano%new(1)
   N%O%new(0) = N%O%new(1)
   N%H%new(0) = N%H%new(1)
-
-  P%micro%new(M+1) = P%micro%old(M+1)
-  P%nano%new(M+1) = P%nano%old(M+1)
-  N%O%new(M+1) = N%O%old(M+1)
-  N%H%new(M+1) = N%H%old(M+1)
+  Sil%new(0) = Sil%new(1)
 
   do xx = 1, D_bins
      Detritus(xx)%D%new(0) = Detritus(xx)%D%new(1)
-     Detritus(xx)%D%new(M+1) = Detritus(xx)%D%old(M+1)
   end do
 
 end subroutine find_new
