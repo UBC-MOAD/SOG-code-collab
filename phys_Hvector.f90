c2dfa16daaa4d424d3112b3e526259c7ecdabb96
Doug Latornell <djl@douglatornell.ca>
2006-09-03 19:01:17 +0000

    Created from U_H.f90.  Code cleaned up to improve readability.  Takes the place of U_H() and scalar_H().

--- phys_Hvector.f90
+++ phys_Hvector.f90
@@ -1,39 +1,39 @@
-SUBROUTINE U_H (M, velold, Gvector, Gvector_o, Gvector_c, Gvector_co, &
-  Bmatrix_o, Hvector)
-  ! subroutine puts vert adv, bottom and surface fluxes (Gvector),
-  ! Coriolis and prressure gradient fluxes (Gvector_c) and appropriate old
-  ! diffusion (Bmatrix_o) into Hvector
+! $Id$
+! $Source$
 
-  USE mean_param, only: trivector
-  USE IMEX_constants, only: a_IMEX1
+subroutine phys_Hvector(M, qty_old, Gvector, Gvector_o, Gvector_c, &
+     Gvector_co, Bmatrix_o, Hvector)
+  ! Put vert adv, bottom and surface fluxes (Gvector),
+  ! Coriolis and pressure gradient fluxes (Gvector_c) and appropriate old
+  ! diffusion (Bmatrix_o) into Hvector.
 
-  IMPLICIT NONE
+  use precision_defs, only: dp
+  use mean_param, only: trivector
+  use IMEX_constants, only: a_IMEX1
 
-  integer, intent(in) :: M
-  double precision, dimension(0:M+1):: velold   ! U%old etc
-  DOUBLE PRECISION, DIMENSION(M), INTENT(IN):: &
+  implicit none
+
+  ! Arguments:
+  integer, intent(in) :: M  ! Number of grid points
+  real(kind=dp), dimension(0:M+1):: qty_old   ! U%old, etc.
+  real(kind=dp), dimension(M), intent(in):: &
        Gvector, Gvector_o, &  ! vert adv & surf/bot fluxes
        Gvector_c, Gvector_co  ! Coriolis and Pressure gradient forces     
-  TYPE(trivector),INTENT(IN)::Bmatrix_o ! old diffusion
-
-  DOUBLE PRECISION, DIMENSION(M), INTENT(OUT)::Hvector  ! zonal vector
+  type(trivector),intent(in)::Bmatrix_o ! old diffusion
+  ! Result:
+  real(kind=dp), dimension(M), intent(out)::Hvector  ! zonal vector
  
-  ! local variables
-  INTEGER::index ! count through vertical
+  ! Local variable:
+  integer :: j ! loop index over depth
 
-  Hvector = 0.
-
-  DO index = 1, M
-     Hvector(index) = velold(index) + &
-          (1.0 - a_IMEX1) * (Gvector_o(index) + Gvector_co(index) + &
-           Bmatrix_o%A(index) * velold(index-1) + &
-           Bmatrix_o%B(index) * velold(index) + &
-           Bmatrix_o%C(index)*velold(index+1)) + &
-           a_IMEX1 * (Gvector(index) + Gvector_c(index))
-  END DO
-
-END SUBROUTINE U_H
-
-
-
-
+  ! *** This can probably be vectorized, but no point until we're inside
+  ! *** a module, and M goes away by virtual of assumed-shape arrays.
+  do j = 1, M
+     Hvector(j) = qty_old(j) + &
+          (1.0 - a_IMEX1) * (Gvector_o(j) + Gvector_co(j) + &
+           Bmatrix_o%A(j) * qty_old(j-1) + &
+           Bmatrix_o%B(j) * qty_old(j) + &
+           Bmatrix_o%C(j)*qty_old(j+1)) + &
+          a_IMEX1 * (Gvector(j) + Gvector_c(j))
+  end do
+end subroutine phys_Hvector
