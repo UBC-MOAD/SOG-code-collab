62a001c3b6965a3eb9f455b9c82702cba589f26c
Doug Latornell <djl@douglatornell.ca>
2006-09-04 19:21:27 +0000

    Cleaned up comments to improve readability.  Changed double precision declarations to real(kid=dp).  Added CVS keywords to top of file.

--- IMEX_constants.f90
+++ IMEX_constants.f90
@@ -1,33 +1,27 @@
-MODULE IMEX_constants
+! $Id$
+! $Source$
 
-	IMPLICIT NONE
+module IMEX_constants
+  ! Parameters that control the types of differencing used by the IMEX
+  ! scheme.  See Ascher et al (1995), SIAM J. Numer Anal. 32(3),
+  ! pp. 797-823
 
-!!!See Ascher et al (1995), SIAM J. Numer Anal. 32(3), pp. 797-823!!!!
+  use precision_defs, only: dp
+  implicit none
 
-!!!!implicit-explicit first order scheme:  a_IMEX1 = 1.0 --> backward Euler in diffusion
-!!!				                   = 0.5 --> Crank-Nicolson in diffusion
-!!!                                                = 0.0 --> forward Euler in diffusion
-!!!                                       Both have forward Euler in explicit  terms (i.e. Coriolis and 
-!!!                                                                                 Boundary conditions)
+  ! Implicit-explicit first order scheme:
+  !   a_IMEX1 = 1.0 --> backward Euler in diffusion
+  !           = 0.5 --> Crank-Nicolson in diffusion
+  !           = 0.0 --> forward Euler in diffusion
+  ! Both have forward Euler in explicit terms (i.e. Coriolis and
+  ! boundary conditions)
+  real(kind=dp), parameter :: a_IMEX1 = 1.0
 
-	DOUBLE PRECISION, PARAMETER::a_IMEX1 = 1.0
-
-!!!!implicit-explicit second order scheme: (a_IMEX2, b_IMEX2) = (0.5, 0) --> CNAB (Crank-Nicolson, Adams-
-!!!!                                                                               Bashforth)
-!!			                                      = (0.5, 1/8) --> MCNAB (modified Crank-Nicolson,
-!!!!                                                             1/8 = 0.125              Adams-Bashforth)
-!!!							      = (0, 1)--> CNLF (Crank-Nicolson, Leap frog)
-!!!!                                                          = (1, 0)--> SBDF (second order backward 
-!!!                                                                             differencing)
-	DOUBLE PRECISION, PARAMETER::a_IMEX2 = 1.0, b_IMEX2 =0.0
-
-END MODULE IMEX_constants
-
-
-
-
-
-
-
-
-
+  ! Implicit-explicit second order scheme: 
+  !   (a_IMEX2, b_IMEX2) = (0.5, 0) --> CNAB (Crank-Nicolson, Adams-Bashforth)
+  !			 = (0.5, 0.125) --> MCNAB (modified Crank-Nicolson, 
+  !                                                Adams-Bashforth)
+  !                      = (0, 1)--> CNLF (Crank-Nicolson, Leap frog)
+  !                      = (1, 0)--> SBDF (second order backward differencing)
+  real(kind=dp), parameter :: a_IMEX2 = 1.0, b_IMEX2 =0.0
+end module IMEX_constants
