2de036ddc2473ea16ca8b96066ddf14e623261dd
Doug Latornell <djl@douglatornell.ca>
2006-10-03 03:32:02 +0000

    Made S_grad_i, the profile of salinity gradient at the grid interface depths an argument of the subroutine.

--- define_flux.f90
+++ define_flux.f90
@@ -7,7 +7,7 @@ module define_flux_mod
 
 contains
 
-  subroutine define_flux(T_grad_i, alpha, beta)
+  subroutine define_flux(T_grad_i, S_grad_i, alpha, beta)
     ! *** What's it do?
 
     use precision_defs, only: dp
@@ -20,7 +20,8 @@ contains
 
     ! Arguments:
     real(kind=dp), dimension(1:) :: &
-         T_grad_i  ! Temperature gradient profile at grid interface depths
+         T_grad_i, &  ! Temperature gradient profile at grid interface depths
+         S_grad_i     ! Salinity gradient profile at grid interface depths
     type(water_property), intent(in) :: &
          alpha, &  ! Thermal expansion coefficient profile arrays
          beta      ! Saline contraction coefficient profile arrays
@@ -44,7 +45,7 @@ contains
           w%b_err(xx) = 0.
        else
           w%t(xx) = -K%t%ML(xx) * (T_grad_i(xx) - gamma%t(xx)) ! (9)
-          w%s(xx) = -K%s%ML(xx) * (S%div_i(xx) - gamma%s(xx))
+          w%s(xx) = -K%s%ML(xx) * (S_grad_i(xx) - gamma%s(xx))
           w%u(xx) = -K%u%ML(xx) * (U%div_i(xx) - gamma%m(xx))
           w%v(xx) = -K%u%ML(xx) * (V%div_i(xx) - gamma%m(xx))
           ! Buoyancy flux by definition given t and s flux
@@ -60,7 +61,7 @@ contains
     do xx = h%i, grid%M
        if (grid%d_i(xx) > h%new) then
           w%t(xx) = -K%t%total(xx) * T_grad_i(xx)
-          w%s(xx) = -K%s%total(xx) * S%div_i(xx)
+          w%s(xx) = -K%s%total(xx) * S_grad_i(xx)
           w%u(xx) = -K%u%total(xx) * U%div_i(xx)
           w%v(xx) = -K%u%total(xx) * V%div_i(xx)
           w%b(xx) = g * (alpha%i(xx) * w%t(xx) - beta%i(xx) * w%s(xx))
