769a00a646afc956fb8cf4ab00b5748ab662ac6d
Doug Latornell <djl@douglatornell.ca>
2006-10-03 03:42:10 +0000

    Finished refactoring of double_diffusion() subroutine.
    Replaced grid in argument list with number of grid points, because
    that is all that is needed in the subroutine.
    
    Replaced sal in the argument list with S_grad_i because S%grad_i is
    the only component of S that is used in the subroutine.
    
    Expanded the nus argument to nu_t_dd, and nu_s_dd to make the
    relationship to K%t%dd, and K%s%dd in SOG.f90 more obvious.
    
    Deleted use statements for grid_mod, mean_param, and surface_forcing
    because the subroutine now uses nothing from any of those modules.
    
    Added more comments about what the code is doing, and references to
    equations in Large, et al (1994).

--- physics_model.f90
+++ physics_model.f90
@@ -23,71 +23,80 @@ module physics_model
 
 contains
 
-  subroutine double_diffusion(mm, T_grad_i, sal, alpha_i, beta_i, nus)
-    ! *** What's it do?
+  subroutine double_diffusion(M, T_grad_i, S_grad_i, alpha_i, beta_i, &
+       nu_t_dd, nu_s_dd)
+    ! Calculate double diffusion mixing.  
+    ! See Large, etal (1994), pp373-374, equations 30 to 34.
     use precision_defs, only: dp
-    use grid_mod, only: grid_
-    use mean_param, only: prop, Knu, div_interface
-!!$    use surface_forcing
+    use io_unit_defs, only: stderr
     implicit none
     ! Arguments:
-    type(grid_), intent(in) :: mm  ! *** Replace this with M
-    real(kind=dp), dimension(1:mm%M), intent(in) :: &
-         T_grad_i  ! Temperature gradient profile at grid interface depths
-    type(prop), intent(inout) :: sal
-    real(kind=dp), dimension(0:mm%M), intent(in) :: &
+    integer, intent(in) :: M  ! Number of grid points
+    real(kind=dp), dimension(1:), intent(in) :: &
+         T_grad_i, &  ! Temperature gradient profile at grid interface depths
+         S_grad_i     ! Salinity gradient profile at grid interface depths
+    real(kind=dp), dimension(0:), intent(in) :: &
          alpha_i, &  ! Thermal expansion coeff profile at interface depths
          beta_i      ! Salinity contraction coeff profile at interface depths
-    ! *** Changed this to K%s%dd, and K%t%dd as output args
-    type(Knu), intent(inout) :: nus
+    real(kind=dp), dimension(1:), intent(out) :: &
+         nu_t_dd, &  ! Thermal diffusivity for double diffusion mixing
+         nu_s_dd     ! Saline diffusivity for double diffusion mixing
 
     ! Local variables:
-    real(kind=dp), dimension(1:mm%M) :: Ri_rho
+    real(kind=dp), dimension(1:M) :: R_rho  ! Double diffusion density ratio
     integer :: k  ! Loop index over depth
+    ! Double diffusion model parameters (see Large, etal (1994) pg 374, fig. 4)
     real(kind=dp), parameter :: &
-         nu_f = 0.001, &    ! Salt fingering constant [m^2/s]
-         Ri_rho_o = 1.9, &  ! *** Something to do with salt fingering
+         nu_f = 10.0d-4, &  ! Maximum salt fingering diffusivity [m^2/s]
+         R_rho_o = 1.9, &   ! Salt fingering limit (nu_s = 0)
          p_2 = 3, &         ! Salt finger diff power constant
-         nu = 1.5d-06       ! Molecular viscosity [m^2/s]
+         nu = 1.5d-06       ! Molecular diffusivity (viscosity) [m^2/s]
 
-    CALL div_interface(mm, sal)
-
-!!$    nus%s%dd = 0.0
-!!$    nus%t%dd = 0.0
-
-      ! Calculate double diffusion denisty ratio (30)
-      Ri_rho = alpha_i(1:) * T_grad_i / (beta_i(1:) * sal%div_i)
+    ! Calculate double diffusion density ratio (Large, et al, (1994) eq'n 30)
+    R_rho = alpha_i(1:) * T_grad_i / (beta_i(1:) * S_grad_i)
       
-      ! *** May be able to use a forall construct here
-      do k = 1, mm%M
-         if (1.0 < Ri_rho(k) &
-              .and. Ri_rho(k) < 2.0 &
-              .and. alpha_i(k) * T_grad_i(k) > 0 &
-              .and. beta_i(k) * sal%div_i(k) > 0.) then  
-            ! Salt fingering
-            if (1.0 < Ri_rho(k) .and. Ri_rho(k) < Ri_rho_o) then
-               nus%s%dd(k) = nu_f * (1.0 - ((Ri_rho(k) - 1.0) &
-                    / (Ri_rho_o - 1.0)) ** 2) ** p_2   ! (31a)
-               nus%t%dd(k) = 0.7 * nus%s%dd(k) ! (31c)            
-            endif ! (31b) not needed, already set to 0
-         else if (0. < Ri_rho(k) &
-              .and. Ri_rho(k) < 1.0 &
-              .and. alpha_i(k) * T_grad_i(k) < 0. &
-              .and. beta_i(k) * sal%div_i(k) < 0.) then   
-            ! Diffusive instability
-            nus%t%dd(k) = nu * 0.909 &
-                 * exp(4.6 * exp(-0.54 * (1.0 / Ri_rho(k) - 1.0))) !(32)
-            if (Ri_rho(k) >= 0.5 .and. Ri_rho(k) < 1.0) then
-               nus%s%dd(k) = nus%t%dd(k) &
-                    * (1.85 - 0.85 / Ri_rho(k)) * Ri_rho(k) !(34)
-            else if (Ri_rho(k) < 0.5) then
-               nus%s%dd(k) = nus%t%dd(k) * 0.15 * Ri_rho(k) ! (34)
-            endif
-         else
-            nus%t%dd(k) = 0.0
-            nus%s%dd(k) = 0.0
-         endif
-      enddo
-    end subroutine double_diffusion
+    do k = 1, M
+       ! Determine if there is double diffision, and if so, what type,
+       ! salt fingering, or diffusive convection
+       if (1.0 < R_rho(k) &
+            .and. R_rho(k) < 2.0 &
+            .and. alpha_i(k) * T_grad_i(k) > 0 &
+            .and. beta_i(k) * S_grad_i(k) > 0.) then  
+          ! Salt fingering
+          if (1.0 < R_rho(k) .and. R_rho(k) < R_rho_o) then
+             ! Large, et al, (1994) eq'n 31a
+             nu_s_dd(k) = nu_f &
+                  * (1.0 - ((R_rho(k) - 1.0) &
+                             / (R_rho_o - 1.0)) ** 2) ** p_2
+          else  ! R_rho >= R_rho_o
+             ! Large, et al, (1994) eq'n 31b
+             nu_s_dd = 0.
+          endif
+          ! Large, et al, (1994) eq'n 31c
+          nu_t_dd(k) = 0.7 * nu_s_dd(k)
+       else if (0. < R_rho(k) &
+            .and. R_rho(k) < 1.0 &
+            .and. alpha_i(k) * T_grad_i(k) < 0. &
+            .and. beta_i(k) * S_grad_i(k) < 0.) then   
+          ! Diffusive convection
+          ! Large, et al, (1994) eq'n 32
+          nu_t_dd(k) = nu * 0.909 &
+               * exp(4.6 * exp(-0.54 * (1.0 / R_rho(k) - 1.0)))
+          ! Large, et al, (1994) eq'n 34
+          if (R_rho(k) >= 0.5 .and. R_rho(k) < 1.0) then
+             nu_s_dd(k) = nu_t_dd(k) &
+                  * (1.85 - 0.85 / R_rho(k)) * R_rho(k) !(34)
+          else if (R_rho(k) < 0.5) then
+             nu_s_dd(k) = nu_t_dd(k) * 0.15 * R_rho(k) ! (34)
+          endif
+       else
+          ! No double diffusion (i.e. temperature and salinity are
+          ! both stabilizing, or water column is convectively
+          ! unstable)
+          nu_t_dd(k) = 0.0
+          nu_s_dd(k) = 0.0
+       endif
+    enddo
+  end subroutine double_diffusion
 
 end module physics_model
