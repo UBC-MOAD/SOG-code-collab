f45ad4e34053d6f5f65d8a8cb9d0e029663854f5
Doug Latornell <djl@douglatornell.ca>
2006-10-05 02:39:21 +0000

    Removed negative checking, and boundary condition setting code related to P%micor, and P%nano in favour of new code temporarily located in SOG.f90

--- find_new.f90
+++ find_new.f90
@@ -6,8 +6,7 @@ subroutine find_new(M)
   ! removing any negative values.  Applies upper boundary conditions
 
   use surface_forcing, only: zero
-  use declarations, only: P1_p, Pnano1_p, Detritus1_p,&
-       P, Detritus, D_bins, neg_count
+  use declarations, only: Detritus1_p, Detritus, D_bins, neg_count
 
   implicit none
 
@@ -18,15 +17,6 @@ subroutine find_new(M)
   INTEGER:: xx, yy
 
   DO yy = 1, M
-     IF (P1_p(yy) < zero) THEN
-        P1_p(yy) = zero
-        neg_count = neg_count + 1
-     END IF
-     IF (Pnano1_p(yy) < zero) THEN
-        Pnano1_p(yy)= zero
-        neg_count = neg_count + 1
-     END IF
-
      DO xx = 1, D_bins
         IF (Detritus1_p(xx,yy) < zero) THEN
            Detritus1_p(xx,yy) = zero
@@ -34,14 +24,7 @@ subroutine find_new(M)
         END IF
         Detritus(xx)%D%new(yy) = Detritus1_p(xx,yy)
      END DO
-
-     P%micro%new(yy) = P1_p(yy)
-     P%nano%new(yy) = Pnano1_p(yy)
-
   END DO
-
-  P%micro%new(0) = P%micro%new(1)
-  P%nano%new(0) = P%nano%new(1)
 
   do xx = 1, D_bins
      Detritus(xx)%D%new(0) = Detritus(xx)%D%new(1)
