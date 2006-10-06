b0cd240c8be19c4eb9298a4323a2e1b9eb5bfd5a
Doug Latornell <djl@douglatornell.ca>
2006-10-06 23:20:28 +0000

    Changed interp_value() from function to subroutine so that it can return the index of the grid point below the interpolated value (j_below) as well as the value.  This allows interp_value() to be used in the new depth_average() function.
    Added new depth_average() function to return the average value of the
    grid point stored quantity qty_g between depths d1, and d2.

--- grid.f90
+++ grid.f90
@@ -21,35 +21,40 @@ module grid_mod
   !
   ! Public Functions:
   !
-  !   interp_value -- Return the value of the quantity in the
-  !                   interp_array that corresponds to the the specified
-  !                   value in the search_array.  This can be used to
-  !                   find the value of a quantity at a specified depth:
-  !                      qty_g = find(d, d_g, qty_g) or
-  !                      qty_i = find(d, d_i, qty_i)
-  !                   or to find the shallowest depth at which a specified
-  !                   quantity value occurs:
-  !                      d = find(q, qty_g, d_g) or
-  !                      d = find(q, qty_i, d_i)
-  !
-  !   interp_i(qty_g) -- Return the interpolated values of a quantity at
-  !                      the grid interface depths from its values at the 
-  !                      grid point depths.
+  !   depth_average(qty_g, d1, d2) -- Return the average value of the
+  !                                   grid point stored quantity qty_g
+  !                                   between depths d1, and d2.
+  !   gradient_g(qty_g) -- Return the values of the gradients at the
+  !                        grid point depths of a quantity stored
+  !                        at the grid point depths.
   !
   !   gradient_i(qty_g) -- Return the values of the gradients at the
   !                        grid layer interface depths of a quantity stored
   !                        at the grid point depths.
   !
-  !   gradient_g(qty_g) -- Return the values of the gradients at the
-  !                        grid point depths of a quantity stored
-  !                        at the grid point depths.
+  !   interp_i(qty_g) -- Return the interpolated values of a quantity at
+  !                      the grid interface depths from its values at the 
+  !                      grid point depths.
   ! 
   ! Public Subroutines:
+  !
+  !   dalloc_grid() -- Deallocate memory from grid arrays.
   !
   !   init_grid(M, grid) -- Allocate memory for grid arrays and
   !                         initialize grid interpolation factor arrays.
   !
-  !   dalloc_grid() -- Deallocate memory from grid arrays.
+  !   interp_value(value, search_array, interp_array, interp_val, j_below)
+  !      -- Calculate the value of the quantity in the interp_array that
+  !         corresponds to the the specified value in the search_array,
+  !         and the index of the grid point or interface immediately below
+  !         the calculated value.  This can be used to find the value of a 
+  !         quantity at a specified depth:
+  !           call interp_value(d, d_g, qty_g, qty_g(d), j_below) or
+  !           call interp_value(d, d_i, qty_i, aty_i(d), j_below)
+  !         or to find the shallowest depth at which a specified quantity
+  !         value occurs:
+  !           call interp_value(q, qty_g, d_g, d(q), j_below) or
+  !           call interp_value(q, qty_i, d_i, d(q), j_below)
 
   use precision_defs, only: dp
   implicit none
@@ -61,9 +66,9 @@ module grid_mod
        ! Variables:
        grid, &
        ! Functions:
-       interp_value, interp_i, gradient_i, gradient_g, &
+       depth_average, gradient_g, gradient_i, interp_i, &
        ! Subroutines:
-       init_grid, dalloc_grid
+       dalloc_grid, init_grid, interp_value
 
   ! Public type definition:
   !
@@ -228,66 +233,79 @@ contains
   end subroutine dalloc_grid
 
 
-  function interp_value(value, search_array, interp_array) result(interp_val)
-    ! Return the value of the quantity in the interp_array that
-    ! corresponds to the the specified value in the search_array.
-    ! This can be used to find the value of a quantity at a specified
-    ! depth:
-    !        qty_g = find(d, d_g, qty_g) or
-    !        qty_i = find(d, d_i, qty_i)
-    ! or to find the shallowest depth at which a specified quantity
-    ! value occurs:
-    !        d = find(q, qty_g, d_g) or
-    !        d = find(q, qty_i, d_i)
+  function depth_average(qty_g, d1, d2) result(avg)
+    ! Return the average value of the grid point stored quantity qty_g
+    ! between depths d1, and d2.
     use io_unit_defs, only: stdout
     implicit none
     ! Arguments:
-    real(kind=dp), intent(in) :: value  ! Depth or quantity value to find
-    real(kind=dp), dimension(1:), intent(in) :: &
-         search_array, &  ! Array to find value in
-         interp_array     ! Array to interpolate over
+    real(kind=dp), dimension(0:), intent(in) :: &
+         qty_g  ! Quantity values at grid point depths
+    real(kind=dp), intent(in) :: &
+         d1, &  ! Beginning of depth range to calculate average over
+         d2     ! End of depth range to calculate average over
     ! Result:
-    real(kind=dp) :: interp_val
+    real(kind=dp) :: avg  ! Average value
     ! Local variables:
-    logical, dimension(1:size(search_array)) :: mask
-    integer :: j_below
+    real(kind=dp) :: &
+         q_d1, &    ! Value of quantity at depth d1
+         q_d2, &    ! Value of quantity at depth d2
+         q_i_d1, &  ! Value of qty at depth of grid interface layer below d1
+         q_i_d2     ! Value of qty at depth of grid interface layer above d2
+    integer :: &
+         j_below_d1, &  ! Index of grid point below depth d1
+         j_below_d2, &  ! Index of grid point below depth d2
+         j_above_d1, &  ! Index of grid point above depth d1
+         j_above_d2, &  ! Index of grid point above depth d2
+         j_junk         ! Unused j_below argument of interp_value()
 
-    ! Mask the search array for values less than the specified
-    ! value, and confirm that the specified value is within the range
-    ! of the search array
-    mask = search_array > value
-    if (any(mask) .and. .not. all(mask)) then
-       ! Find the index of the search array element below the
-       ! specified value
-       j_below = minval(indices, mask)
-       ! Interpolate the value of the quantity in interp_array that
-       ! corresponds to the specified value in the search array
-       interp_val = interp_array(j_below-1) &
-            + (interp_array(j_below) - interp_array(j_below-1)) &
-            * (value - search_array(j_below-1)) &
-            / (search_array(j_below) - search_array(j_below-1))
-    else
-       write(stdout, *) "Warning: value = ", value, &
-            " out of range in interp_value()"
-       interp_val = 9999999999.
+    ! Validate the depth arguments
+    if (d1 < grid%d_g(0) .or. d2 > grid%d_g(grid%M + 1)) then
+       write(stdout, *) "Warning: depth(s) out of range in depth_average(): ",&
+            "(", d1, ", ", d2, ")"
     endif
-  end function interp_value
+    ! Find the values of the quantity at the depth limits, and the
+    ! indices of the grid points immediately below those depths
+    call interp_value(d1, grid%d_g, qty_g, q_d1, j_below_d1)
+    call interp_value(d2, grid%d_g, qty_g, q_d2, j_below_d2)
+    ! Adjust the j_below value for the fact that interp_value operates
+    ! on 1-based index arrays, and qty_g arrays are 0-based, and set the
+    ! indices of the grid points immediately above d1, and d2
+    j_below_d1 = j_below_d1 - 1
+    j_below_d2 = j_below_d2 - 1
+    j_above_d1 = j_below_d1 - 1
+    j_above_d2 = j_below_d2 - 1
+    ! Calculate the depth weighted average value of the quantity
+    ! Portion between d1 and nearest grid layer interface below
+    call interp_value(grid%d_i(j_above_d1), grid%d_g, qty_g, &
+         q_i_d1, j_junk)
+    avg = (q_d1 + q_i_d1) / 2 * (grid%d_i(j_above_d1) - d1)
+    ! Portion between whole grid layer interfaces
+    avg = avg + dot_product(qty_g(j_below_d1:j_above_d2), &
+         grid%i_space(j_below_d1:j_above_d2))
+    ! Portion below last grid layer interface and d2
+    call interp_value(grid%d_i(j_above_d2), grid%d_g, qty_g, &
+         q_i_d2, j_junk)
+    avg = avg + ((q_d2 + q_i_d2) / 2 * (d2 - grid%d_i(j_above_d2)))
+    avg = avg / (d2 - d1)
+  end function depth_average
 
 
-  function interp_i(qty_g) result(qty_i)
-    ! Return the interpolated values of a quantity at the grid interface
-    ! depths from its values at the grid point depths.
+  function gradient_g(qty_g) result(grad_g)
+    ! Return the values of the gradients at the grid point
+    ! depths of a quantity stored at the grid point depths.
+    ! *** This function does not work for an unevenly spaced grid
     implicit none
-    ! Arguments:
+    ! Argument:
     ! Quantity values at grid point depths
     real(kind=dp), dimension(0:), intent(in) :: qty_g
     ! Result:
-    ! Quantity values at grid interface depths
-    real(kind=dp), dimension(0:grid%M) :: qty_i
+    ! Values of the gradients at grid point depths
+    real(kind=dp), dimension(1:grid%M) :: grad_g
 
-    qty_i = qty_g(0:size(qty_g) - 2) * above_g%factor &
-         + qty_g(1:) * below_g%factor
-  end function interp_i
+    grad_g = (qty_g(0:grid%M-1) - qty_g(2:grid%M+1)) &
+         / (grid%g_space(0:grid%M-1) + grid%g_space(1:))
+  end function gradient_g
 
 
   function gradient_i(qty_g) result(grad_i)
@@ -306,20 +324,68 @@ contains
   end function gradient_i
 
 
-  function gradient_g(qty_g) result(grad_g)
-    ! Return the values of the gradients at the grid point
-    ! depths of a quantity stored at the grid point depths.
-    ! *** This function does not work for an unevenly spaced grid
+  function interp_i(qty_g) result(qty_i)
+    ! Return the interpolated values of a quantity at the grid interface
+    ! depths from its values at the grid point depths.
     implicit none
-    ! Argument:
+    ! Arguments:
     ! Quantity values at grid point depths
     real(kind=dp), dimension(0:), intent(in) :: qty_g
     ! Result:
-    ! Values of the gradients at grid point depths
-    real(kind=dp), dimension(1:grid%M) :: grad_g
+    ! Quantity values at grid interface depths
+    real(kind=dp), dimension(0:grid%M) :: qty_i
 
-    grad_g = (qty_g(0:grid%M-1) - qty_g(2:grid%M+1)) &
-         / (grid%g_space(0:grid%M-1) + grid%g_space(1:))
-  end function gradient_g
+    qty_i = qty_g(0:size(qty_g) - 2) * above_g%factor &
+         + qty_g(1:) * below_g%factor
+  end function interp_i
+
+
+  subroutine interp_value(value, search_array, interp_array, &  ! in
+       interp_val, j_below)                                     ! out
+    ! Calculate the value of the quantity in the interp_array that
+    ! corresponds to the the specified value in the search_array,
+    ! and the index of the grid point or interface immediately below
+    ! the calculated value.  This can be used to find the value of a 
+    ! quantity at a specified depth:
+    !   call interp_value(d, d_g, qty_g, qty_g(d), j_below) or
+    !   call interp_value(d, d_i, qty_i, aty_i(d), j_below)
+    ! or to find the shallowest depth at which a specified quantity
+    ! value occurs:
+    !   call interp_value(q, qty_g, d_g, d(q), j_below) or
+    !   call interp_value(q, qty_i, d_i, d(q), j_below)
+    use io_unit_defs, only: stdout
+    implicit none
+    ! Arguments:
+    real(kind=dp), intent(in) :: value  ! Depth or quantity value to find
+    real(kind=dp), dimension(1:), intent(in) :: &
+         search_array, &  ! Array to find value in
+         interp_array     ! Array to interpolate over
+    real(kind=dp), intent(out) :: interp_val
+    integer, intent(out) :: j_below
+    ! Local variables:
+    logical, dimension(1:size(search_array)) :: mask
+
+    ! Mask the search array for values greater than or equal to the
+    ! specified value, and confirm that the specified value is within
+    ! the range of the search array.  We need to add the epsilon of
+    ! value so that value = 0 is handled properly.
+    mask = search_array >= (value + epsilon(value))
+    if (any(mask) .and. .not. all(mask)) then
+       ! Find the index of the search array element below the
+       ! specified value
+       j_below = minval(indices, mask)
+       ! Interpolate the value of the quantity in interp_array that
+       ! corresponds to the specified value in the search array
+       interp_val = interp_array(j_below-1) &
+            + (interp_array(j_below) - interp_array(j_below-1)) &
+            * (value - search_array(j_below-1)) &
+            / (search_array(j_below) - search_array(j_below-1))
+    else
+       write(stdout, *) "Warning: value = ", value, &
+            " out of range in interp_value()"
+       interp_val = 9999999999.
+       j_below = 999999
+    endif
+  end subroutine interp_value
 
 end module grid_mod
