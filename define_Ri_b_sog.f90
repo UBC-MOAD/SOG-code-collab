e04e037da4630bc578d0fd29741e2a765474e951
Doug Latornell <djl@douglatornell.ca>
2006-10-07 00:06:11 +0000

    Changed density argument to rho array, and average density calculation to use depth_average() function.  This eliminates the need for density as a type(prop) variable, and (finally) completes the refactoring of water column properties.  Yahoo!!

--- define_Ri_b_sog.f90
+++ define_Ri_b_sog.f90
@@ -1,7 +1,7 @@
 ! $Id$
 ! $Source$
 
-subroutine define_Ri_b_sog(d, hh, surf_h, Uu, Vv, dens, Rib, &
+subroutine define_Ri_b_sog(d, hh, surf_h, Uu, Vv, rho, Rib, &
      Vt_sq, N2)
   ! Calculate the profile of the bulk Richardson number, which is
   ! used to find the mixing layer depth.  See Large, etal (1994) pp
@@ -9,7 +9,7 @@ subroutine define_Ri_b_sog(d, hh, surf_h, Uu, Vv, dens, Rib, &
   ! *** Argument order needs to be standardized
 
   use precision_defs, only: dp
-  use grid_mod, only: grid_
+  use grid_mod, only: grid_, depth_average
   USE mean_param, only: height, prop
   USE surface_forcing
 
@@ -17,24 +17,25 @@ subroutine define_Ri_b_sog(d, hh, surf_h, Uu, Vv, dens, Rib, &
   type(grid_), intent(in) :: d
   type(height), intent(in) :: hh 
   type(height), intent(out) :: surf_h
-  type(prop), intent(in out) :: Uu, Vv, dens
+  real(kind=dp), dimension(0:d%M+1), intent(in) :: rho
+  type(prop), intent(in out) :: Uu, Vv
   real(kind=dp), dimension(1:d%M), intent(in) :: Vt_sq
   real(kind=dp), dimension(0:d%M), intent(out) :: Rib
   real(kind=dp), dimension(0:d%M+1), intent(in) :: N2
  
   ! Local variables:
   real(kind=dp), dimension(0:d%M+1) :: test_vector, test_vector2
-  integer :: yy, ymin
-  real(kind=dp) ::Ribmin
+  integer :: yy, ymin, j_below
+  real(kind=dp) ::Ribmin, rho_avg
 
   surf_h%new = ep*hh%new
        
   call average(d,Uu,surf_h) !test conv  
   call average(d,Vv,surf_h) !test conv 
-  call average(d,dens,surf_h)
+  rho_avg = depth_average(rho, 0.0d0, surf_h%new)
 
   test_vector = (Uu%avg - Uu%new)**2.0 + (Vv%avg - Vv%new)**2.0 
-  test_vector2 =  -g / dens%new(0) * (dens%avg - dens%new) !Bb%avg - Bb%new  
+  test_vector2 =  -g / rho(0) * (rho_avg - rho) !Bb%avg - Bb%new  
          
   Rib = 0.0
   Ribmin = 1000.
