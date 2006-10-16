14faaccdb8278552b4fc9e830faeff6fd12446f3
Doug Latornell <djl@douglatornell.ca>
2006-10-16 12:10:07 +0000

    Removed Coriolis factor from argument list.  It is now a parameter value supplied by the physics_model module.

--- Coriolis_and_pg.f90
+++ Coriolis_and_pg.f90
@@ -9,21 +9,18 @@ module Coriolis_and_pg_mod
 
 contains
 
-  subroutine Coriolis_and_pg(f, dt, vel, pbgrad, Gvector_c)
+  subroutine Coriolis_and_pg(dt, vel, pbgrad, Gvector_c)
     ! Calculate the Coriolis and baroclinic pressure gradient
     ! components of the G vector for the specified velocity component.
-
+    use physics_model, only: f
     implicit none
-
     ! Arguments:
-    real(kind=dp), intent(in) :: f
     real(kind=dp), intent(in) :: dt
     real(kind=dp), dimension(0:), intent(in)::vel
     real(kind=dp), dimension(1:), intent(in)::pbgrad
     real(kind=dp), dimension(1:), intent(out)::Gvector_c
 
     Gvector_c = (f * vel(1:) - pbgrad) * dt
-
   end subroutine Coriolis_and_pg
 
 end module Coriolis_and_pg_mod
