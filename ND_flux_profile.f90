ce68b4954a95c414961342e232d212c00ff66594
Doug Latornell <djl@douglatornell.ca>
2006-10-16 12:35:10 +0000

    Changed source of parameter values declaration for KPP nondimensional flux profiles coefficients from surface_forcing to turbulence module.
    Changed argument names to make them match those used in SOG.f90.

--- ND_flux_profile.f90
+++ ND_flux_profile.f90
@@ -1,47 +1,50 @@
 ! $Id$
 ! $Source$
 
-SUBROUTINE ND_flux_profile(mm,L,phi1)
+SUBROUTINE ND_flux_profile(grid, L_star, phi)
   ! Calculate the nondimensional flux profiles per Large, etal (1994),
   ! App. B
-  ! *** Cleanup needed.  Constants should be declared here instead of
-  ! *** in surface forcing, etc.
+  ! *** Cleanup needed.
      
   use precision_defs, only: dp
   use grid_mod, only: grid_
-      USE mean_param, only: MS
-      USE surface_forcing
+  use turbulence, only: &
+       xsi_m, &  ! Max xsi value of the -1/3 power law regime of phi%m
+       xsi_s, &  ! Max xsi value of the -1/3 power law regime of phi%s
+       a_m,   &  ! Coefficient of phi%m in 1/3 power law regime
+       a_s,   &  ! Coefficient of phi%s in 1/3 power law regime
+       c_m,   &  ! Coefficient of phi%m in 1/3 power law regime
+       c_s       ! Coefficient of phi%s in 1/3 power law regime
+  USE mean_param, only: MS
+  implicit none
+  ! Arguments:
+  type(grid_), intent(in) :: grid
+  real(kind=dp), intent(in) :: L_star
+  type(MS), intent(out) :: phi
+  ! Local variables:
+  integer :: y
 
-      IMPLICIT NONE
+     !Define phi%m%value
+      phi%m%value = 0.
+      phi%s%value = 0.
 
-      TYPE(grid_), INTENT(IN)::mm
-      TYPE(MS), INTENT(OUT)::phi1
-      real(kind=dp), intent(in) :: L  !L_star
-
-      INTEGER::y
-
-     !Define phi1%m%value
-      
-      phi1%m%value = 0.
-      phi1%s%value = 0.
-
-      DO y = 0,mm%M
-         IF (mm%d_i(y)/L >= 0.)THEN ! stable (defined as functions of d, the grid)
-            phi1%m%value(y) = 1.0+5.0*mm%d_i(y)/L ! (B1a)
-         ELSE IF (mm%d_i(y)/L < 0. .AND. xsi_m <= mm%d_i(y)/L)THEN
-            phi1%m%value(y) = (1.0-16.0*mm%d_i(y)/L)**(-1.0/4.0) ! (B1b)
+      DO y = 0,grid%M
+         IF (grid%d_i(y)/L_star >= 0.)THEN ! stable (defined as functions of d, the grid)
+            phi%m%value(y) = 1.0+5.0*grid%d_i(y)/L_star ! (B1a)
+         ELSE IF (grid%d_i(y)/L_star < 0. .AND. xsi_m <= grid%d_i(y)/L_star)THEN
+            phi%m%value(y) = (1.0-16.0*grid%d_i(y)/L_star)**(-1.0/4.0) ! (B1b)
          ELSE
-            phi1%m%value(y) = (a_m - c_m*mm%d_i(y)/L)**(-1.0/3.0) ! (B1c)
+            phi%m%value(y) = (a_m - c_m*grid%d_i(y)/L_star)**(-1.0/3.0) ! (B1c)
          END IF
 
-      !Define phi1%s%value
+      !Define phi%s%value
 
-         IF (mm%d_i(y)/L >= 0.)THEN
-           phi1%s%value(y) = 1.0+5.0*mm%d_i(y)/L ! (B1a)
-         ELSE IF (mm%d_i(y)/L < 0. .AND. xsi_s <= mm%d_i(y)/L)THEN
-           phi1%s%value(y) = (1.0-16.0*mm%d_i(y)/L)**(-1.0/2.0) ! (B1d)
+         IF (grid%d_i(y)/L_star >= 0.)THEN
+           phi%s%value(y) = 1.0+5.0*grid%d_i(y)/L_star ! (B1a)
+         ELSE IF (grid%d_i(y)/L_star < 0. .AND. xsi_s <= grid%d_i(y)/L_star)THEN
+           phi%s%value(y) = (1.0-16.0*grid%d_i(y)/L_star)**(-1.0/2.0) ! (B1d)
          ELSE
-            phi1%s%value(y) = (a_s - c_s*mm%d_i(y)/L)**(-1.0/3.0) ! (B1e)
+            phi%s%value(y) = (a_s - c_s*grid%d_i(y)/L_star)**(-1.0/3.0) ! (B1e)
          END IF
       END DO
 
