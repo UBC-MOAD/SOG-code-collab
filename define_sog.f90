b133192d20fef43304a1dea93418a0bde43f159e
Doug Latornell <djl@douglatornell.ca>
2006-10-27 15:06:24 +0000

    Change related to RHS arrays refactoring.
    Replaced code that copied biology Gvector% components to Gvector_o%
    components with a call to new_to_old_bio_RHS() in SOG.f90.  That call
    will eventually go into a master new_to_old subroutine that will
    probably come from the yet to be written numerics module.

--- define_sog.f90
+++ define_sog.f90
@@ -5,7 +5,7 @@ SUBROUTINE define_sog(timestep)
   
   !!store previous time_steps in old (n)
   use core_variables, only: U, V, T, S
-  USE declarations, only: D_bins, h, &
+  USE declarations, only: h, &
        Bmatrix, Bmatrix_o, & 
        Gvector, Gvector_o, Gvector_c, Gvector_co
 
@@ -34,20 +34,9 @@ SUBROUTINE define_sog(timestep)
          Gvector_o%v = Gvector%v
          Gvector_o%s = Gvector%s
          Gvector_o%t = Gvector%t
-
-         Gvector_o%p%micro = Gvector%p%micro
-         Gvector_o%p%nano = Gvector%p%nano
-         Gvector_o%n%o = Gvector%n%o
-         Gvector_o%n%h = Gvector%n%h
-         Gvector_o%si = Gvector%si
         
          Gvector_co%u = Gvector_c%u
          Gvector_co%v = Gvector_c%v
-
-         DO kk = 1, D_bins
-            Gvector_o%d(kk)%bin = Gvector%d(kk)%bin 
-         END DO
-         
       END IF
 
          U%old = U%new
