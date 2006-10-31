658b1fac6d1611b0518f2851ff158db0dde4af0f
Susan Allen <sallen@eos.ubc.ca>
2006-10-31 14:29:31 +0000

    Added code for Pnano and Pmicro river fluxes.

--- freshwater.f90
+++ freshwater.f90
@@ -9,6 +9,10 @@ module freshwater
   private
   public :: freshwater_bio
 
+  real(kind=dp), parameter :: river_nitrate = 6.4
+  real(kind=dp), parameter :: river_Pmicro = 0.
+  real(kind=dp), parameter :: river_Pnano = 0.
+
 contains
 
   subroutine init_freshwater
@@ -31,7 +35,11 @@ contains
     real (kind=dp), dimension(0:), intent(out) :: aflux
 
     if (qty.eq."nitrate") then
-       aflux = - Fw * (6.4 - current_value)
+       aflux = - Fw * (river_nitrate - current_value)
+    elseif (qty.eq."Pmicro") then
+       aflux = - Fw * (river_Pmicro - current_value)
+    elseif (qty.eq."Pnano") then
+       aflux = - Fw * (river_Pnano - current_value)
     else
        write (*,*) "problems in freshwater"
        stop
