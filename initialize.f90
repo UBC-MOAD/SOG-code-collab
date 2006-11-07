0b052aff52b493b3a21d0e9752004b45f9af0795
Doug Latornell <djl@douglatornell.ca>
2006-11-07 13:07:05 +0000

    Deleted unecessary initializations of 4 variables: month, dummy_time, neg_count, and time_step.

--- initialize.f90
+++ initialize.f90
@@ -8,17 +8,12 @@ SUBROUTINE initialize
       IMPLICIT NONE
 
       time = dble(t_o)
-      dummy_time = 0.
       day = day_o   !initialize julian day
-     ! month = month_o   !KC june 28, 2004
        year = year_o
 
       day_time = time
-      time_step = 1
 
       j_gamma = 0  !h_gamma = 0 or wt_r contribution to gamma%t vanishes
-
-      neg_count = 0  ! counts number of times scheme biology becomes negative
 
       f_ratio = 0.
       null_vector = 0.
