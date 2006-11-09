c1bd12cf4f6fb74b59f5bd5ca45874e268f3504f
Doug Latornell <djl@douglatornell.ca>
2006-11-09 17:41:21 +0000

    Moved Coriolis factor(f) code here from physics_model module to resolve circular use statement issues.  That necessitated creating an init_constants() subroutine, because pgf90 has a bug that prevents f from being declared as a parameter due to its use of the sin() instrinsic.

--- fundamental_constants.f90
+++ fundamental_constants.f90
@@ -7,6 +7,8 @@ module fundamental_constants
   !
   ! Public Parameters:
   !
+  !   f -- Coriolis factor
+  !
   !   g -- Acceleration due to gravity [m/s^2]
   !
   !   latitude -- Station S3 latitude [deg]
@@ -19,16 +21,28 @@ module fundamental_constants
   private
   public :: &
        ! Parameter values:
+       f,        &  !  Coriolis factor
        g,        &  ! Acceleration due to gravity [m/s^2]
        latitude, &  ! Station S3 latitude [deg]
-       pi           ! Ratio of circumference to diameter of a circle [-]
+       pi,       &  ! Ratio of circumference to diameter of a circle [-]
+       ! Subroutine:
+       init_constants
 
-  ! Parameter Value Declarations:
-  !
-  ! Public:
+  ! Public parameter declarations:
+  real(kind=dp) :: &
+       f  ! Coriolis factor (would be a parameter but for a pgf90 bug)
   real(kind=dp), parameter :: &
        g = 9.80665, &                  ! Acceleration due to gravity [m/s^2]
        latitude = 49. + 7.517 / 60., & ! Station S3 latitude [deg]
        pi = 3.141592653589793
 
+contains
+
+  subroutine init_constants()
+    ! Coriolis factor
+    ! *** This must be calculated because pgf90 will not accept an
+    ! *** intrinsic in parameter statement
+    f = 2. * (2. * pi / 86400.) * sin(pi * latitude / 180.)
+  end subroutine init_constants
+  
 end module fundamental_constants
