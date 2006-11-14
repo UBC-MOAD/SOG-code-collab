301c61439a27d2b1052e9189358e74f909a8a4e4
Doug Latornell <djl@douglatornell.ca>
2006-11-14 10:28:06 +0000

    Changes related to turbulence refactoring:
    Deleted declaration of old turbulent kinematic fluxes (w).  It has
    been refactored into the turbulence module.

--- declarations.f90
+++ declarations.f90
@@ -21,7 +21,6 @@ module declarations
   character*4 :: cruise_id  ! four number code that labels the start cruise
 
   TYPE(Knu)::K 
-  TYPE(flux)::w
   TYPE(height)::h, surface_h
   TYPE(MST)::gamma,G_shape
   TYPE(MS)::phi,omega
