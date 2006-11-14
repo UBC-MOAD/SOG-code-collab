604b9635e6a0c4ed5dae58ac5cdd558cb86aa0ad
Doug Latornell <djl@douglatornell.ca>
2006-11-14 10:44:09 +0000

    Changes related to turbulence refactoring:
    Deleted old surface momentum turbulent kinematic flux variables (w_u &
    w_v) from arguments list of wind_stress().  Replaced them with a "use
    turbulence, only: wbar" statement.  Changed all instances of w_u & w_v
    to wbar%u(0) & wbar%v(0), respectively.
    
    Refactored the zero wind-speed test in wind_speed() to use
    epsilon(0.0d0) for robustness.
    
    Did documentation and whitespace cleanup on
    longwave_latent_sensible_heat().  Deleted old surface thermal
    turbulent kinematic flux variable (w_t) from arguments list.  Replaced
    it with a "use turbulence, only: wbar" statement.  Changed all
    instances of w_t to wbar%t(0).

--- air_sea_fluxes.f90
+++ air_sea_fluxes.f90
@@ -31,7 +31,7 @@ module air_sea_fluxes
   
 contains
   
-  subroutine wind_stress (unow, vnow, rho, w_u, w_v)
+  subroutine wind_stress(unow, vnow, rho)
     ! Calculate the wind-stress.
     !
     ! This calculates the values of the wbar%u(0), and wbar%v(0)
@@ -53,7 +53,6 @@ contains
          vnow, &  ! 325 degree wind component (along-strait)
          rho      ! surface water density (doesn't have to be current
                   ! value)
-    real(kind=dp), intent(out) :: w_u, w_v ! surface momentum flux components
     ! Local variables:
     real(kind=dp) :: &
          C_D,      &  ! drag coefficient (Large and Pond)
@@ -64,93 +63,104 @@ contains
     UU = sqrt(unow ** 2 + vnow ** 2)
     ! We need to handle dead, flat, calm as a special case to avoid a
     ! divide-by-zero in the drag coefficient formula.
-    if (UU /= 0.) then
+    if (UU < epsilon(0.0d0)) then
+       stress_u = 0.
+       stress_v = 0.
+    else
        C_D = 1.0d-3 * (2.70 / UU + 0.142 + 0.0764 * UU)
        stress_u = unow / UU * C_D * rho_atm * UU ** 2
        stress_v = vnow / UU * C_D * rho_atm * UU ** 2
-    else
-       stress_u = 0.
-       stress_v = 0.
     endif
     ! Calculate the surface turbulent kinematic flux momentum
     ! components (Large, et al, (1994), eq'ns A2a & A2b).
-    w_u = -stress_u / rho
-    w_v = -stress_v / rho
+    wbar%u(0) = -stress_u / rho
+    wbar%v(0) = -stress_v / rho
   end subroutine wind_stress
 
+  
+  subroutine longwave_latent_sensible_heat(cf, atemp, humid, T_o, &  ! in
+       rho_o, Cp_o)                                                  ! in
+    ! Calculate the surface turbulent heat flux.
+    !
+    ! This calculates the values of the wbar%t(0) variable that is
+    ! declared in the turbulence module.
 
-  subroutine longwave_latent_sensible_heat(cf, atemp, humid, T_o, rho_o, Cp_o,&
-       w_t)
-
+    ! Elements from other modules:
+    !
+    ! Type Definitions:
+    use precision_defs, only: dp
+    ! Variable Declarations:
+    use turbulence, only: &
+         wbar  ! Turbulent kinematic flux profile arrays
+    ! Functions:
     use unit_conversions, only: KtoC
+
     implicit none
 
     ! Arguments
-    real(kind=sp),intent(in) :: cf, & ! cloud fraction in 0-1
-         atemp, & ! air temperature
-         humid ! humidity (%)
-    real(kind=dp), intent(in) :: T_o, & ! sea surface water temperature
-         rho_o, & ! surface water density
-         Cp_o ! surface water specific heat
-    real(kind=dp), intent(out) :: w_t ! heat flux at ocean surface
-    ! local variables
+    real(kind=sp),intent(in) :: &
+         cf,    & ! Cloud fraction [0 to 1]
+         atemp, & ! Air temperature [K]
+         humid, & ! humidity [%]
+         T_o,   & ! Sea surface water temperature [K]
+         rho_o, & ! Sea surface water density  [kg/m^3]
+         Cp_o     ! Sea surface water specific heat [kJ/kg-K]
+    ! Local parameter values and variables:
+    !
     ! Longwave radiation
-    real(kind=dp), parameter :: r = 0.03, &
-         Ce = 9.37d-06, &
-         sigma = 5.6697d-08, & ! Stefan Boltzmann constant
-         epsilon_w = 0.96 ! surface emissivity in IR portion of the spectrum
-    real(kind=dp) :: lw_in, & ! downward long wave radiation
-         lw_out, & ! upward emission of long wave radiation 
-         lw_net ! net longwave radiation
-
+    real(kind=dp), parameter :: &
+         r = 0.03,           &
+         Ce = 9.37d-06,      &
+         sigma = 5.6697d-08, &  ! Stefan Boltzmann constant
+         epsilon_w = 0.96       ! surface emissivity in IR portion of
+                                ! the spectrum Local variables:
+    real(kind=dp) :: &
+         lw_in,  &  ! Downward long wave radiation
+         lw_out, &  ! Upward emission of long wave radiation 
+         lw_net     ! Net longwave radiation
+    !
     ! Sensible heat flux
-    real(kind=dp), parameter :: Cs = 1.3e-3, & ! sensible heat transfer coeff.
-         Cp = 1003 ! specific heat of air
-    real(kind=dp) :: h_sens ! sensible heat flux
-
+    real(kind=dp), parameter :: &
+         Cs = 1.3d-3, &  ! Sensible heat transfer coefficient
+         Cp = 1003.      ! Specific heat of air
+    real(kind=dp) :: &
+         h_sens  ! Sensible heat flux
+    !
     ! Latent heat flux 
-    ! vapour pressure consts
-    real(kind=dp), parameter :: a = 7.5, &
-         b = 237.3, &
-         c = 0.7858
-    ! latent heat consts
-    real (kind=dp), parameter :: CL = 1.3e-3, &
+    real(kind=dp), parameter :: &
+         a = 7.5,      &  ! Vapour pressure consts
+         b = 237.3,    &
+         c = 0.7858,   &
+         CL = 1.3e-3,  &  ! Latent heat consts
          LE = 2.453d06
+    real(kind=dp) :: &
+         ea,       &  ! Vapour pressure [mb]
+         es,       &  ! Saturated vapous pressure [mb]
+         h_latent, &  ! Latent heat
+         Q_t          ! Surface heat flux [W/m^2]
 
-    real(kind=dp) :: ea, & ! vapour pressure
-         es, & ! saturated vapous pressure
-         h_latent ! latent heat
-
-    real(kind=dp) :: Q_t ! surface heat flux
-
-    !downward radiation from atmosphere
-    !there are several different ways to calculate this
+    ! Downward radiation from atmosphere
+    ! *** There are several different ways to calculate this
     lw_in = (1 - r) * (1 +0.17 * cf**2) * Ce * atemp**2 &
          * sigma * atemp**4  
-
-    !upward emission of radiation from earth's surface, stull page 48
+    ! Upward emission of radiation from earth's surface, stull page 48
     lw_out = -epsilon_w * sigma * T_o**4                      
-
     lw_net = lw_in + lw_out
-
     ! Sensible heat flux
     h_sens = Cs * rho_atm * Cp * UU * (atemp - T_o) 
-
-    ! vapour pressure
+    ! Vapour pressure in mb
     ea = (humid / 100.) * &
-         exp(2.303 * ((a * KtoC(atemp) / ( KtoC(atemp) + b)) + c))  ! mb
-    !saturated vapour pressure !*** same const as above??
+         exp(2.303 * ((a * KtoC(atemp) / ( KtoC(atemp) + b)) + c))
+    !Saturated vapour pressure 
+    ! *** Same const as above??
     es = exp(2.3026 * ((a * KtoC(T_o) / (KtoC(T_o) + b)) + c))
-
-    ! latent heat
-    h_latent = (0.622 / 1013.) * CL * rho_atm * LE * UU * (ea-es)
-    h_latent = max(h_latent,0.d0)
-
-    Q_t = lw_net + h_sens + h_latent    ! W/m2
-
-    ! Temperature (eq'n A2c)
-    w_t = -Q_t / (rho_o * Cp_o) ! w%t(0)
-
+    ! Latent heat
+    h_latent = (0.622 / 1013.) * CL * rho_atm * LE * UU * (ea - es)
+    h_latent = max(h_latent, 0.d0)
+    ! Surface heat flux in W/m^2
+    Q_t = lw_net + h_sens + h_latent
+    ! Surface turbulent heat flux (Large, et al (1994), eq'n A2c)
+    wbar%t(0) = -Q_t / (rho_o * Cp_o)
   end subroutine longwave_latent_sensible_heat
 
 end module air_sea_fluxes
