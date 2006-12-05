37a2cc852a6b8c2d2c65dbb81b342f7d925ba81c
Doug Latornell <djl@douglatornell.ca>
2006-12-05 15:52:06 +0000

    Removed the critical Richardson number (Ri_c) from the public parameters list because it is no longer used outside this module.
    Added dalloc_mixing_layer_variables() to the public subroutines list
    so that it can be used by the physics_model module.
    
    Added private variable declarations and memory allocation for profile
    arrays for the local buoyancy frequency (N), the turbulent velocity
    chear squared (V_t_2), and the bulk Richardson number (Ri_b).
    
    Streamlined the argument list for the find_mixing_layer_depth()
    subroutine.  Added a call to it to calc_Ri_b(), the new bulk
    Richardson number profile calculation subroutine.
    
    Added new subroutines calc_Ri_b(), and calc_turbulent_vel_shear() that
    contain refactored version of the code in define_Ri_b_sog.f90, and
    def_v_t_sog.f90, respectively, along with some code form SOG.f90.

--- mixing_layer.f90
+++ mixing_layer.f90
@@ -6,10 +6,6 @@ module mixing_layer
   ! subroutines related to the mixing layer depth calculation in the
   ! SOG code.
   !
-  ! Public Parameters:
-  !
-  !   Ri_c -- Critical Richardson number
-  !
   ! Public Variables:
   !
   !   h -- Mixing layer depth values & indices
@@ -24,19 +20,20 @@ module mixing_layer
   !                                grid point & grid layer interface
   !                                immediately below the mixing layer
   !                                depth.
+  !
+  !   dalloc_mixing_layer_variables -- Deallocate memory from mixing
+  !                                    layer depth variables.
 
   use precision_defs, only: dp
   implicit none
 
   private
   public :: &
-       ! Parameter values:
-       Ri_c, &    ! Critical Richardson number
        ! Variables:
        h, &  ! Mixing layer depth values & indices
        ! Subroutines:
        init_mixing_layer, find_mixing_layer_depth, &
-       find_mixing_layer_indices
+       find_mixing_layer_indices, dalloc_mixing_layer_variables
 
   ! Type Definitions:
   !
@@ -62,6 +59,12 @@ module mixing_layer
   ! Public
   type(mixing_layer_depth) :: &
        h  ! Mixing layer depth values & indices
+  
+  ! Private:
+  real(kind=dp), dimension(:), allocatable :: &
+       N, &      ! Local buoyancy frequency profile
+       V_t_2, &  ! Profile of the square fo the turbulent velocity shear
+       Ri_b      ! Bulk Richardson number profile
 
 contains
 
@@ -70,6 +73,8 @@ contains
     ! Argument:
     integer, intent(in) :: M  ! Number of grid points
 
+    ! Allocate memory for mixing layer depth related arrays
+    call alloc_mixing_layer_variables(M)
     ! Initialize the mixing layer depth, and the indices of the grid
     ! point & grid layer interface immediately below it
     h%new = 2.0d0
@@ -77,32 +82,29 @@ contains
   end subroutine init_mixing_layer
 
 
-  subroutine find_mixing_layer_depth(grid, Ri_b, Bf, year, day, day_time, &
-       count)
+  subroutine find_mixing_layer_depth(year, day, day_time, count)
     ! Find the mixing layer depth.  See Large, etal (1994) pp 371-372.
 
     ! Elements from other modules:
     !
     ! Type Definitions:
     use precision_defs, only: dp
-    use grid_mod, only: grid_
     ! Parameter Value Declarations:
     use io_unit_defs, only: stdout
     use fundamental_constants, only: f
     ! Variables
-    use turbulence, only: u_star, L_mo
+    use grid_mod, only: &
+         grid  ! Grid parameters and arrays
+    use turbulence, only: &
+         u_star, &  ! Turbulent friction velocity
+         L_mo       ! Monin_Obukhov length scale
+    use declarations, only: Bf  ! *** Should come from somewhere else
     ! Subroutines:
     use grid_mod, only: interp_value
 
     implicit none
 
     ! Arguments:
-    type(grid_), intent(in) :: &
-         grid  ! Grid parameters and arrays
-    real(kind=dp), dimension(0:), intent(in) :: &
-         Ri_b  ! Bulk Richardson no.
-    real(kind=dp), intent(in) :: &
-         Bf  ! Surface buoyancy flux
     integer, intent(in) :: &
          year, &  ! Year for flagging mixing too deep events
          day,  &  ! Year-day  for flagging mixing too deep events
@@ -114,6 +116,8 @@ contains
     real(kind=dp) :: &
          d_Ekman  ! Ekman depth [m]
 
+    ! Calculate the profile of the bulk Richardson number.
+    call calc_Ri_b()
     ! Find the depth at which the bulk Richardson number exceeds the
     ! critical value
     call interp_value(Ri_c, 0, Ri_b, grid%d_g(0:grid%M), h%new, h%g)
@@ -151,6 +155,130 @@ contains
   end subroutine find_mixing_layer_depth
 
 
+  subroutine calc_Ri_b()
+    ! Calculate the profile of the bulk Richardson number.
+
+    ! Elements from other modules:
+    !
+    ! Type Definitions:
+    use precision_defs, only: dp
+    ! Functions:
+    use grid_mod, only: depth_average
+    ! Parameter Values:
+    use fundamental_constants, only: g
+    use turbulence, only: epsiln
+    ! Variables:
+    use grid_mod, only: &
+         grid  ! Grid parameters and arrays
+    use water_properties, only: &
+         rho  ! Density profile
+    use core_variables, only: &
+         U, &  ! Cross-strait (35 deg) velocity component arrays
+         V     ! Along-strait (305 deg) velocity component arrays
+         
+
+    ! Local Variables:
+    real(kind=dp) :: &
+         d_surf,   & ! Surface layer extent [m]
+         U_r, V_r, & ! Average value of velocity components in the
+                     ! surface layer.
+         rho_r       ! Average value of the density in the surface
+                     ! layer.
+    integer :: &
+         M  ! Number of grid points (to improve readability of
+            ! vectorized code).
+
+
+    ! Note that we are going to make use of the relation B = (alpha *
+    ! T - beta * S) = -g * rho / rho_o here to express the buoyancy
+    ! terms in Large, et al (1994), eqn 21 in terms of rho.
+
+    ! Calculate the average values of the density, and the velocity
+    ! components in the surface layer.
+    d_surf = epsiln * h%new
+    rho_r = depth_average(rho%g, 0.0d0, d_surf)
+    U_r = depth_average(U%new, 0.0d0, d_surf)
+    V_r = depth_average(V%new, 0.0d0, d_surf)
+
+    ! Calculate the profile of turbulent velocity shear
+    call calc_turbulent_vel_shear()
+
+    ! Calculate the profile of the bulk Richardson number
+    !
+    ! Note that we are going to make use of the relation B = (alpha *
+    ! T - beta * S) = -g * rho / rho_o here to express the buoyancy
+    ! terms in Large, et al (1994), eqn 21 in terms of rho.
+    M = grid%M
+    Ri_b(0) = 0.0d0
+    Ri_b(1:M) = (-g * (rho_r - rho%g(1:M)) / rho%g(0)) * grid%d_g(1:M)  &
+         / ((U_r - U%new(1:M)) ** 2 + (V_r - V%new(1:M)) ** 2 &
+            + V_t_2(1:M) + epsilon(U_r))
+  end subroutine calc_Ri_b
+
+
+  subroutine calc_turbulent_vel_shear()
+    ! Calculate the profile of turbulent velocity shear for use in the
+    ! bulk Richardson number profile calculation.
+
+    ! Elements from other modules:
+    !
+    ! Type Definitions:
+    use precision_defs, only: dp
+    ! Functions:
+    use turbulence, only: w_scale, nondim_scalar_flux
+    ! Parameter Values:
+    use fundamental_constants, only: &
+         g  ! Acceleration due to gravity
+    use turbulence, only: &
+       c_s,    &  ! Coefficient of non-dimensional turbulent scalar
+                  ! flux profile in 1/3 power law regime
+       kapa,   &  ! von Karman constant
+       epsiln     ! Non-dimensional extent of the surface layer
+    ! Variables:
+    use grid_mod, only: &
+         grid  ! Grid parameters and arrays
+    use water_properties, only: &
+         rho  ! Density profile
+    use turbulence, only: &
+         wbar  ! Turbulent kinematic flux profile arrays
+    use declarations, only: Bf  ! *** Should come from somewhere else
+
+    ! Local Parameters:
+    real(kind=dp), parameter :: &
+         Cv = 1.5d0  ! Ratio of interior local buoyancy frequency to
+                     ! same at entrainment depth.  *** Large, et al
+                     ! (1994), table 1 recommends a value of 1.5 to
+                     ! 1.6
+
+    ! Local Variables:
+    real(kind=dp) :: &
+         beta_t, &  ! Ratio of entrainment flux to surface buoyancy flux
+         w_s        ! Value of scalar turbulent velocity scale
+    integer :: &
+         j  ! Index over grid depth
+    
+    ! Calculate the profile of the local buoyancy frequency at the
+    ! grid point depths.
+    N = sqrt(-(g / rho%g(1:grid%M)) * rho%grad_g(1:grid%M))
+    ! Set the value of the ratio of entrainment flux to surface
+    ! buoyancy flux, and calculate the turbulent velocity shear
+    ! profile (Large, et al (1994), eqn 23).
+    if (wbar%b(0) > 0.0d0) then
+       ! Constant when we have convection (see Large, et al (1994),
+       ! table 1).
+       beta_t = -0.2d0
+       do j = 1, grid%M
+          w_s = w_scale(h%new, grid%d_g(j), Bf, nondim_scalar_flux, c_s)
+          ! Large, et al (1994), eqn 23.
+          V_t_2(j) = Cv * sqrt(-beta_t / (c_s * epsiln)) &
+               * grid%d_g(j) * N(j) * w_s / (Ri_c * kapa ** 2)
+       enddo
+    else
+       V_t_2 = 0.0d0
+    endif
+  end subroutine calc_turbulent_vel_shear
+
+
   subroutine find_mixing_layer_indices()
     ! Set the value of the indices of the grid point & grid layer
     ! interface immediately below the mixing layer depth.
@@ -178,7 +306,7 @@ contains
   end subroutine find_mixing_layer_indices
 
 
-  subroutine alloc_mixing_layer(M)
+  subroutine alloc_mixing_layer_variables(M)
     ! Allocate memory for core variables arrays.
     use malloc, only: alloc_check
     implicit none
@@ -188,10 +316,41 @@ contains
     integer           :: allocstat  ! Allocation return status
     character(len=80) :: msg        ! Allocation failure message prefix
 
-  end subroutine alloc_mixing_layer
+    msg = "Local buoyancy frequency profile array"
+    allocate(N(1:M), &
+         stat=allocstat)
+    call alloc_check(allocstat, msg)
+    msg = "Square of turbulent velocity shear profile array"
+    allocate(V_t_2(1:M), &
+         stat=allocstat)
+    call alloc_check(allocstat, msg)
+    msg = "Bulk Richardson number profile array"
+    allocate(Ri_b(0:M), &
+         stat=allocstat)
+    call alloc_check(allocstat, msg)
+  end subroutine alloc_mixing_layer_variables
 
 
-  subroutine dalloc_mixing_layer
-  end subroutine dalloc_mixing_layer
+  subroutine dalloc_mixing_layer_variables()
+    ! Deallocate memory for turbulence quantities.
+    use malloc, only: dalloc_check
+    implicit none
+    ! Local variables:
+    integer           :: dallocstat  ! Deallocation return status
+    character(len=80) :: msg         ! Deallocation failure message prefix
+
+    msg = "Local buoyancy frequency profile array"
+    deallocate(N, &
+         stat=dallocstat)
+    call dalloc_check(dallocstat, msg)
+    msg = "Square of turbulent velocity shear profile array"
+    deallocate(V_t_2, &
+         stat=dallocstat)
+    call dalloc_check(dallocstat, msg)
+    msg = "Bulk Richardson number profile array"
+    deallocate(Ri_b, &
+         stat=dallocstat)
+    call dalloc_check(dallocstat, msg)
+  end subroutine dalloc_mixing_layer_variables
 
 end module mixing_layer
