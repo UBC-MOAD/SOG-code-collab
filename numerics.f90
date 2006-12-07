97d86041eb98522d79483dc069e2b5906d87e0cd
Doug Latornell <djl@douglatornell.ca>
2006-12-07 13:46:54 +0000

    Added declarations for a bunch of variables refactored in from SOG.f90: startDatetime, year, day, day_time, time, dt, and steps. Declared max_iter to replace niter, vel_scale to replace uscale, and h_scale to replace hscale.  Added new variables new_weight, and prev_weight.
    Added new subroutines init_numerics(), read_numerics_params(), and
    dalloc_numerics_variables().  Their purposes should be pretty self
    evident.

--- numerics.f90
+++ numerics.f90
@@ -10,33 +10,76 @@ module numerics
   !
   !   tridiag -- Tridiagonal matrix arrays.
   !
-  ! Public Parameters:
-  !
-  !   spam -- Description
-  !
   ! Public Variables:
   !
-  !   eggs -- Description
+  !   startDatetime -- Date/time of initial conditions
+  !
+  !   year -- Year counter
+  !
+  !   day --  Year-day counter
+  !
+  !   day_time -- Day-sec counter
+  !
+  !   time -- Time counter through run; seconds since
+  !           startDatetime%day_sec
+  !
+  !   dt -- Time step [s]
+  !
+  !   steps --  Number of time steps in the main time loop
+  !
+  !   max_iter -- Maximum number of iterations allowed for implicit
+  !               solver loop.
+  !
+  !   new_weight, prev_weight -- Weighting factors for blending of
+  !                              values from current and previous
+  !                              implicit solver iterations.
+  !
+  !   vel_scale, h_scale -- Scale factors used to normalize the
+  !                         velocity components, and mixing layer
+  !                         depth values to calculate the convergence
+  !                         metrics for the implicit solver loop.
   !
   ! Public Subroutines:
   !
   !   check_negative -- Check for negative values in a vector.  Output
   !                     a message if a negative value is found, and
   !                     optionally stop execution; default is to stop.
+  !
+  !   dalloc_numerics_variables -- Deallocate memory for numerics
+  !                                variables.
 
   use precision_defs, only: dp
+  use datetime, only: datetime_
   implicit none
 
   private
   public :: &
        ! Type definitions:
        tridiag, &
-!!$       ! Parameter values:
-!!$       spam, &
-!!$       ! Variables:
-!!$       eggs, &
+       ! Variables:
+       startDatetime, &   ! Date/time of initial conditions
+       year, &   ! Year counter
+       day,  &   ! Year-day counter
+       day_time, &  ! Day-sec counter
+       time, &  ! Time counter through run; seconds since
+                ! startDatetime%day_sec
+       dt, &      ! Time step [s]
+       steps, &   ! Number of time steps in the main time loop
+       max_iter, &  ! Maximum number of iterations allowed for
+                    ! implicit solver loop.
+       hprev, &  ! Values of h%new, u%new and v%new from previous
+       Uprev, &  ! iteration for blending with new value to stabilize
+       Vprev, &  ! convergence of the implicit solver loop
+       new_weight, prev_weight, &  ! Weighting factors for convergence
+                                   ! stabilization blending.
+       vel_scale, h_scale,  &  ! Scale factors used to normalize the
+                               ! velocity components, and mixing layer
+                               ! depth values to calculate the
+                               ! convergence metrics for the implicit
+                               ! solver loop.
+       del, &  ! Convergence metrics for implicit solver loop.
        ! Subroutines:
-       check_negative
+       init_numerics, check_negative, dalloc_numerics_variables
 
   ! Type Definitions:
   !
@@ -50,15 +93,111 @@ module numerics
           sup      ! Super-diagonal vector of a tridiagonal matrix
   end type tridiag
   !
-  ! Private to module:
+  ! Private:
+  !
+  ! Convergence metrics for implicit solver loop.
+  type :: converg_metrics
+     real(kind=dp) :: &
+          U, &  ! Cross-strait (35 deg) velocity component arrays
+          V, &  ! Along-strait (305 deg) velocity component arrays
+          h     ! Mising layer depth
+  end type converg_metrics
 
-  ! Parameter Value Declarations:
+  ! Variable Declarations:
   !
   ! Public:
+  type(datetime_) :: &
+       startDatetime   ! Date/time of initial conditions
+  ! *** These should be replaced with a datetime structure
+  integer :: &
+       year, &   ! Year counter
+       day   ! Year-day counter
+  real(kind=dp) :: &
+       day_time, &  ! Day-sec counter
+       time  ! Time counter through run; seconds since
+             ! startDatetime%day_sec
+  real(kind=dp) :: &
+       dt      ! Time step [s]
+  integer :: &
+       steps  ! Number of time steps in the main time loop
+  integer :: &
+       max_iter  ! Maximum number of iterations allowed for implicit
+                 ! solver loop.
+  real(kind=dp) :: &
+       hprev  ! Value of h%new from previous iteration for blending
+              ! with new value to stabilize convergence of the
+              ! implicit solver loop.
+  real(kind=dp), dimension (:), allocatable :: &
+       Uprev, Vprev  ! Values of u%new and v%new from previous
+                     ! iteration for blending with new value to
+                     ! stabilize convergence of the implicit solver
+                     ! loop.
+  real(kind=dp)  :: &
+       new_weight,  &  ! Weighting factors for blending of
+       prev_weight, &  ! values from current and previous implicit
+                       ! solver iterations.  Goal of blending is to
+                       ! stabilize convergence.  It is applied to
+                       ! mixing layer depth (h), and velocity
+                       ! components (U & V).
+       vel_scale,   &  ! Scale factors used to normalize the velocity 
+       h_scale         ! components, and mixing layer depth values to
+                       ! calculate the convergence metrics for the
+                       ! implicit solver loop.
+  type(converg_metrics) :: &
+       del  ! Convergence metrics for implicit solver loop.
   !
-  ! Private to module:
+  ! Private:
+  real(kind=dp) :: &
+       t_f  ! Run duration [s]  *** should be replaced with endDatetime
 
 contains
+
+  subroutine init_numerics(M)
+    ! Allocate memory for numerics arrays, and read parameter values
+    ! from the infile.
+
+    ! Subroutines from other modules:
+    use datetime, only: calendar_date, clock_time
+    
+    implicit none
+
+    ! Argument:
+    integer, intent(in) :: M  ! Number of grid points
+
+    ! Allocate memory for numerics quantity arrays
+    call alloc_numerics_variables(M)
+    ! Read numerics parameter values from the infile.
+    call read_numerics_params()
+    ! Calculate the month number and month day for output file headers
+    call calendar_date(startDatetime)
+    call clock_time(startDatetime)
+    ! Initialize the time counters
+    year = startDatetime%yr
+    day = startDatetime%yr_day
+    day_time = dble(startDatetime%day_sec)
+    time = dble(startDatetime%day_sec)
+    ! Calculate the number of time steps for the run (note that int()
+    ! rounds down)
+    steps = 1 + int((t_f - dble(startDatetime%day_sec)) / dt)
+  end subroutine init_numerics
+
+
+  subroutine read_numerics_params()
+    ! Read the fresh water parameter values from the infile.
+    use input_processor, only: getpari, getpard
+    implicit none
+
+    ! Initial conditions date/time for the run
+    startDatetime%yr = getpari("year_o")
+    startDatetime%yr_day = getpari("yr_day_o")
+    startDatetime%day_sec = getpari("t_o")
+    ! Run duration, and time step
+    t_f = getpard("run_dur")
+    dt = getpard("dt")
+    ! Maximum number of iterations allowed for implicit solver loop.
+    max_iter = getpari('max_iter')     
+  end subroutine read_numerics_params
+
 
   subroutine check_negative(lbound, vector, msg, day, time, fatal)
     ! Check for negative values in a vector.  Output a message if a
@@ -118,6 +257,38 @@ contains
        endif
     endif
   end subroutine check_negative
+
+
+  subroutine alloc_numerics_variables(M)
+    ! Allocate memory for numerics quantity arrays.
+    use malloc, only: alloc_check
+    implicit none
+    ! Argument:
+    integer, intent(in) :: M  ! Number of grid points
+    ! Local variables:
+    integer           :: allocstat  ! Allocation return status
+    character(len=80) :: msg        ! Allocation failure message prefix
+
+    msg = "Velocity component profile arrays from previous iteration"
+    allocate(Uprev(0:M+1), Vprev(0:M+1), &
+         stat=allocstat)
+    call alloc_check(allocstat, msg)
+  end subroutine alloc_numerics_variables
+
+
+  subroutine dalloc_numerics_variables()
+    ! Deallocate memory for numerics quantity arrays.
+    use malloc, only: dalloc_check
+    implicit none
+    ! Local variables:
+    integer           :: dallocstat  ! Deallocation return status
+    character(len=80) :: msg         ! Deallocation failure message prefix
+
+    msg = "Velocity component profile arrays from previous iteration"
+    deallocate(Uprev, Vprev, &
+         stat=dallocstat)
+    call dalloc_check(dallocstat, msg)
+  end subroutine dalloc_numerics_variables
 
 end module numerics
        
