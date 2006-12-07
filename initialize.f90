f87230a41726af99c8f13e356c21fc8867d4d0e8
Doug Latornell <djl@douglatornell.ca>
2006-12-07 13:31:52 +0000

    Deleted initialization of variables year, day, day_time, and time. That job has been refactored into the numerics module.

--- initialize.f90
+++ initialize.f90
@@ -7,12 +7,6 @@ SUBROUTINE initialize
 
       IMPLICIT NONE
 
-      time = dble(t_o)
-      day = day_o   !initialize julian day
-       year = year_o
-
-      day_time = time
-
       j_gamma = 0  !h_gamma = 0 or wt_r contribution to gamma%t vanishes
 
       f_ratio = 0.
