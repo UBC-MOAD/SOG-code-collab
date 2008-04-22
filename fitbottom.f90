7baca4c0278f213dda65fd6af48bf0ec1f54a814
Megan Wolfe <mwolfe@eos.ubc.ca>
2008-04-22 17:48:26 +0000

    - Added initialization for fitbottom in subroutine init_fitbottom() - Added logical 'temp_constant' which is read in from the in file that determines if the temperature of the bottom water coming up is constant or not - Shifted the equation for temperature so that T = To + dT/dt * (t-2002) +c2*cos(...) .... This centers the time scale around 2002

--- fitbottom.f90
+++ fitbottom.f90
@@ -30,37 +30,51 @@ module fitbottom
   character(len=12), dimension(NQ), parameter :: quantity &
        = ['salinity', 'temperature', 'chl fluor', 'nitrate', 'silicon', &
           'ammonium', 'plank ratio']
-  ! Unless otherwise specified: tit coefficients (see fitbottom.py and 
-  ! fitted.m in
-  ! sog-forcing/bottom for details of how these coefficient values
-  ! were derived.
-  real(kind=dp), dimension(5, NQ), parameter :: c &
-       = reshape([ &
-          ! Salinity (constant, seasonal and biseasonal components)
-          29.62605865d0, 0.10374454d0, -0.03562458d0, -0.14156091d0, &
-          -0.06348989d0, &
-          ! Temperature (constant, seasonal and biseasonal components)
-          9.34995751d0, -0.27245442d0, -0.90151197d0, 0.17933473d0, &
-          -0.05590939d0, &
-          ! Phytoplankton from fluor (constant and seasonal)
-          0.58316137d0, -0.11206845d0, 0.26241523d0, 0.d0, 0.d0, &
-          ! Nitrate (constant) ! from salinity versus nitrate fit Nov 06 LB 91
-          30.5d0, 0.d0, 0.d0, 0.d0, 0.d0, & 
-          ! Silicon (constant) ! from salinity versus silicon fit NOv 06 LB 91
-          54.0d0, 0.d0, 0.d0, 0.d0, 0.d0, &
-          ! Ammonium (constant) ! from salinity versus nitrate fit Nov 06 LB 91
-          1.0d0, 0.d0, 0.d0, 0.d0, 0.d0, &
-          ! Ratio of pico/micro plankton (constant and biseasonal)
-          1.25948868d0, 0.d0, 0.d0, 0.97697686d0, 0.46289294d0 &
-         ], [5, NQ])
 
+    ! Unless otherwise specified: tit coefficients (see fitbottom.py and 
+    ! fitted.m in
+    ! sog-forcing/bottom for details of how these coefficient values
+    ! were derived.
+    real(kind=dp), dimension(6, NQ) :: c &
+         = reshape([ &
+         ! Salinity (constant, seasonal and biseasonal components)
+         29.62605865d0, 0.10374454d0, -0.03562458d0, -0.14156091d0, &
+         -0.06348989d0,0.d0, &
+         ! Temperature (constant, seasonal and biseasonal components)
+         9.3044d0, 0.06384430d0, -0.84712324d0,-0.05355254d0, &
+         -0.05775216d0,0.00007993d0, &
+         ! Phytoplankton from fluor (constant and seasonal)
+         0.58316137d0, -0.11206845d0, 0.26241523d0, 0.d0, 0.d0,0.d0, &
+         ! Nitrate (constant) ! from salinity versus nitrate fit Nov 06 LB 91
+         30.5d0, 0.d0, 0.d0, 0.d0, 0.d0,0.d0, & 
+         ! Silicon (constant) ! from salinity versus silicon fit NOv 06 LB 91
+         54.0d0, 0.d0, 0.d0, 0.d0, 0.d0,0.d0, &
+         ! Ammonium (constant) ! from salinity versus nitrate fit Nov 06 LB 91
+         1.0d0, 0.d0, 0.d0, 0.d0, 0.d0,0.d0, &
+         ! Ratio of pico/micro plankton (constant and biseasonal)
+         1.25948868d0, 0.d0, 0.d0, 0.97697686d0, 0.46289294d0, 0.d0 &
+         ], [6, NQ])
+    ! Flag for constant bottom temperature
+    logical :: temp_constant  !is the temperature coming up through the 
+                              !bottom constant?
 contains
+
+  subroutine init_fitbottom()
+    ! Read the bottom temperature constraints
+    use input_processor, only: getparl
+    implicit none
+
+    ! Is the temperature coming up through the bottom constant?
+    temp_constant = getparl('temp_constant')
+  end subroutine init_fitbottom
 
   function bottom_value (arg, qty) result(value)
     ! Calculate the value of the specified quantitity at the bottom
     ! grid boundary for the given value of arg.
     use precision_defs, only: dp
     use io_unit_defs, only: stdout
+    use fundamental_constants, only: pi
+    
     implicit none
     ! Arguments:
     real(kind=dp), intent(in):: arg
@@ -69,6 +83,16 @@ contains
     real(kind=dp) :: value
     ! Local variable:
     integer:: index
+    
+
+    
+    ! Determine if the temperature of the water coming up from the bottom boundary is 
+    ! constant or not.
+    
+    if(temp_constant) then
+       c(2,6) = 0.d0
+    endif
+
 
     if (qty == quantity(1)) then
        index = 1
@@ -91,11 +115,11 @@ contains
     endif
 
     value = c(1,index) + c(2,index)*cos(arg) + c(3,index)*sin(arg) &
-         + c(4,index)*cos(2.*arg) + c(5,index)*sin(2.*arg)
+         + c(4,index)*cos(3.*arg) + c(5,index)*sin(3.*arg) + c(6,index)*((arg*365.25) /( 2 * pi)) 
   end function bottom_value
 
 
-  subroutine bot_bound_time (day, day_time, &
+  subroutine bot_bound_time (year,day, day_time, &
        Tbot, Sbot, Nobot, Sibot, Nhbot, Pmbot, Pnbot, Ppbot, uZbot)
     ! Calculate the values at the bottom of the grid for those
     ! quantities that we have data for from an annual fit.
@@ -105,15 +129,19 @@ contains
     use forcing, only: vary_forcing, vary
     implicit none
     ! Arguments:
-    integer, intent(in) :: day
+    integer, intent(in) :: year,day
     real(kind=dp), intent(in) :: day_time
     real(kind=dp), intent(out) :: Tbot, Sbot, Nobot, Sibot, Nhbot, Pmbot, &
          Pnbot, Ppbot, uZbot
     ! Local variables:
-    real(kind=dp) :: arg, chl, ratio
-    
-    arg = 2 * pi * (day + day_time / 86400.) / 365.25
-    
+    real(kind=dp) :: arg, chl, ratio, yeartime
+
+    yeartime = (year-2002) + day/365.25 + day_time/86400/365.25
+
+    arg = 2 * pi * yeartime
+
+  
+
     Tbot = bottom_value(arg, 'temperature')          ! in Celcius
     if (vary%temperature%enabled .and. .not.vary%temperature%fixed) then
        Tbot = CtoK(Tbot) + vary%temperature%addition
