a947861c6f380bad03df685539cc74ae3018b8fe
Doug Latornell <djl@douglatornell.ca>
2009-03-08 00:33:53 +0000

    infile reader for SOG buildbot.
    Read the specified SOG infile and return the output file names separated by newlines.
    
    test_infile_reader.py is the test suite for this script.

---
+++ tests/tools/read_infile.py
@@ -0,0 +1,59 @@
+"""infile reader for SOG buildbot.
+
+Read the specified SOG infile and return the output file names
+separated by newlines.
+
+test_infile_reader.py is the test suite for this script.
+
+:Author: Doug Latornell <dlatorne@eos.ubc.ca>
+:Created: 2009-03-07
+"""
+import re
+import sys
+
+
+def read_infile(argv):
+    """infile reader for SOG buildbot.
+
+    Read the specified SOG infile and return the output file names
+    separated by newlines.
+    """
+    if len(argv) != 2:
+        return_code = 1
+        print 'usage: %(prog)s infile' % {'prog':argv[0]}
+        raise SystemExit(return_code)
+    outfile_keys = ['std_phys_ts_out',
+                    'user_phys_ts_out',
+                    'std_bio_ts_out',
+                    'user_bio_ts_out',
+                    'haloclinefile',
+                    'profile_base',
+                    'Hoffmueller file']
+    p = re.compile(r'"\s')
+    try:
+        infile = open(argv[1]).readlines()
+    except IOError, msg:
+        return_code = 1
+        print msg
+        raise SystemExit(return_code)
+    for i, line in enumerate(infile):
+        # Skip empty lines and comments
+        if line.startswith('\n') or line.startswith('!'):
+            continue
+        # Split line into [key, value, comment]
+        split_line = p.split(line)
+        if split_line[0].strip('"') in outfile_keys:
+            if split_line[1].strip('"').strip().rstrip('\n'):
+                # Value on the same line as key
+                print split_line[1].strip('"')
+            else:
+                # Value on line after key
+                print p.split(infile[i+1])[0].strip().strip('"')
+        return_code = 0
+    raise SystemExit(return_code)
+
+
+
+if __name__== '__main__':
+    read_infile(sys.argv)
+
---
+++ tests/tools/test_read_infile.py
@@ -0,0 +1,157 @@
+"""Test suite for SOG buildbot infile reader.
+
+Use `python test_read_infile.py` to run the test suite.
+
+:Author: Doug Latornell <dlatorne@eos.ubc.ca>
+:Created: 2009-03-07
+"""
+import sys
+import StringIO
+import unittest
+import tempfile
+from read_infile import read_infile
+
+
+class Test_check_diff(unittest.TestCase):
+    def assertRaises(self, excClass, callableObj, *args, **kwargs):
+        """Monkey-patch unittest.assertRaises() to return exception
+        instance.
+        """
+        try:
+            callableObj(*args, **kwargs)
+        except excClass, exc_instance:
+            return exc_instance
+        else:
+            if hasattr(excClass,'__name__'): excName = excClass.__name__
+            else: excName = str(excClass)
+            raise self.failureException, "%s not raised" % excName
+
+
+    def setUp(self):
+        """Pre-test setup.
+        """
+        # Capture stdout
+        sys.stdout = StringIO.StringIO()
+
+
+    def tearDown(self):
+        """Post-test tear-down.
+        """
+        # Restore stdout to default
+        sys.stdout = sys.__stdout__
+
+
+    def test_too_few_args(self):
+        """print usage and exit with status=1 if too few arguments.
+        """
+        argv = 'read_infile.py'.split()
+        exception = self.assertRaises(SystemExit, read_infile, argv)
+        self.assertEqual(exception.code, 1)
+        self.assertEqual(sys.stdout.getvalue(), 
+                         'usage: %(prog)s infile\n' % {'prog':argv[0]})
+
+
+    def test_too_many_args(self):
+        """print usage and exit with status=1 if too many arguments.
+        """
+        argv = 'read_infile.py infile extra'.split()
+        exception = self.assertRaises(SystemExit, read_infile, argv)
+        self.assertEqual(exception.code, 1)
+        self.assertEqual(sys.stdout.getvalue(), 
+                         'usage: %(prog)s infile\n' % {'prog':argv[0]})
+
+
+    def test_bad_file(self):
+        """print error and exit with status=1 if file can't be read.
+        """
+        argv = 'read_infile.py badfile'.split()
+        exception = self.assertRaises(SystemExit, read_infile, argv)
+        self.assertEqual(exception.code, 1)
+        self.assertEqual(sys.stdout.getvalue(), 
+                        "[Errno 2] No such file or directory: 'badfile'\n")
+
+
+    def test_1_line_params(self):
+        """print outfiles and exit with status=0 for 1 line param files.
+        """
+        test_file = tempfile.NamedTemporaryFile()
+        test_file.write("""\
+"dt"		900	 "time step [s]"
+! Implicit solver iteration limit
+"max_iter"		30	"implicit solver max iterations"
+
+
+! Time series output files
+"std_phys_ts_out" "timeseries/std_phys_nov04-test.out" "standard physics timeseries"
+"user_phys_ts_out" "timeseries/user_phys_nov04-test.out" "user-defined physics timeseries"
+
+! Profiles output
+"noprof"	1		"no. of profiles to print"
+! *** It would be nice to replace the yr-day and day-sec lists with a list
+! *** of dates/times for profiles output
+"profday"	303		"yr-day for profile"
+"proftime"	43200.		"day-sec for profile"
+"haloclinefile"	"profiles/halo-nov04-test.out"	"file for halocline results"
+"profile_base"	"profiles/nov04-test"	"profile file base (datetime will be added)"
+! Hoffmueller diagram output (a collection of profiles at time intervals
+! for contour or colourmap plotting)
+"Hoffmueller file"	"profiles/hoff-nov04-test.dat"	"file for Hoffmueller results"\
+""")
+        test_file.flush()
+        argv = ['read_infile.py', test_file.name]
+        exception = self.assertRaises(SystemExit, read_infile, argv)
+        self.assertEqual(exception.code, 0)
+        self.assertEqual(sys.stdout.getvalue(), """\
+timeseries/std_phys_nov04-test.out
+timeseries/user_phys_nov04-test.out
+profiles/halo-nov04-test.out
+profiles/nov04-test
+profiles/hoff-nov04-test.dat
+""")
+
+
+    def test_multiline_params(self):
+        """print outfiles and exit with status=0 for multiline param files.
+        """
+        test_file = tempfile.NamedTemporaryFile()
+        test_file.write("""\
+"dt"		900	 "time step [s]"
+! Implicit solver iteration limit
+"max_iter"		30	"implicit solver max iterations"
+
+
+! Time series output files
+"std_phys_ts_out"
+        "timeseries/std_phys_nov04-test.out" 
+        "standard physics timeseries"
+"user_phys_ts_out" "timeseries/user_phys_nov04-test.out" "user-defined physics timeseries"
+
+! Profiles output
+"noprof"	1		"no. of profiles to print"
+! *** It would be nice to replace the yr-day and day-sec lists with a list
+! *** of dates/times for profiles output
+"profday"	303		"yr-day for profile"
+"proftime"	43200.		"day-sec for profile"
+"haloclinefile"	"profiles/halo-nov04-test.out"	"file for halocline results"
+"profile_base"	
+        "profiles/nov04-test"	
+	"profile file base (datetime will be added)"
+! Hoffmueller diagram output (a collection of profiles at time intervals
+! for contour or colourmap plotting)
+"Hoffmueller file"	"profiles/hoff-nov04-test.dat"	"file for Hoffmueller results"\
+""")
+        test_file.flush()
+        argv = ['read_infile.py', test_file.name]
+        exception = self.assertRaises(SystemExit, read_infile, argv)
+        self.assertEqual(exception.code, 0)
+        self.assertEqual(sys.stdout.getvalue(), """\
+timeseries/std_phys_nov04-test.out
+timeseries/user_phys_nov04-test.out
+profiles/halo-nov04-test.out
+profiles/nov04-test
+profiles/hoff-nov04-test.dat
+""")
+
+
+if __name__ == '__main__':
+    unittest.main()
