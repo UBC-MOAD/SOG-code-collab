11d49fe06007771ccc93efafda4e5a79ce9c7e43
Doug Latornell <djl@douglatornell.ca>
2009-07-05 23:49:22 +0000

    Corrected name of allocation status variable to eliminate unused variable warnings from g95.

--- allocate1.f90
+++ allocate1.f90
@@ -1,7 +1,7 @@
 ! $Id$
 ! $Source$
 
-subroutine allocate1(M, STAT)
+subroutine allocate1(M, stat)
 
       use mean_param
       use declarations
@@ -10,22 +10,22 @@ subroutine allocate1(M, STAT)
      
       ! Arguments:
       integer, intent(in) :: M  ! Number of grid points
-      integer, dimension(20), intent(out) :: STAT  ! Memory allocation status
+      integer, dimension(20), intent(out) :: stat  ! Memory allocation status
 
       ALLOCATE(&
            Q_n(0:M), &
            I(0:M), I_par(0:M), &
-           STAT = alloc_stat(6)) 
+           STAT = stat(6)) 
                      
       ALLOCATE(Gvector%s(M),Gvector%t(M),Gvector%u(M),Gvector%v(M),&
-               STAT = alloc_stat(8))
+               STAT = stat(8))
                
       ALLOCATE(micro%growth%light(M), micro%growth%new(M), &
            nano%growth%light(M), nano%growth%new(M), &
            pico%growth%light(M), pico%growth%new(M), &
            micro%Nlimit(M), nano%Nlimit(M), pico%Nlimit(M), &
            wupwell(0:M), &
-           STAT = alloc_stat(10))
+           STAT = stat(10))
 
 END SUBROUTINE allocate1
 
