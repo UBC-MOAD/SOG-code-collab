128c3acfeb2310a593c54d16d6da28a2a29c6fba
Doug Latornell <djl@douglatornell.ca>
2009-07-23 13:50:21 +0000

    Refactored the code that reads the bottom boundary condition value coefficients into the c matrix in init_fitbottom().  It now reads directly into c, eliminating the need for the split local variable.  Oddly, this seems to have resolved the apparent uninitialized variable issue that was causing the g95-compiled code to give randomly varying results from run to run.  The uninitialized variable issue had been tracked back to init_mixing_layer(), called by init_physics(), just after init_fitbottom().
    Changed a bunch of numerical constants in bottome_value to be explicitly double precision (e.g. changed 2 * pi to 2.0d0 * pi).  One might think that this would have been a more likely contributing factor to the uninitialized variable issue, but bottom_value() isn't called until the end of the time loop.

--- fitbottom.f90
+++ fitbottom.f90
@@ -46,33 +46,20 @@ contains
 
   subroutine init_fitbottom()
     ! Read the bottom temperature constraints
-    use input_processor, only: getparl,getpardv
+    use input_processor, only: getparl, getpardv
     implicit none
-
-    ! Local variable
-    
-    ! fraction to split NQ variables into
-    real (kind=dp) :: split(7)
 
     ! Is the temperature coming up through the bottom constant?
     temp_constant = getparl('temp_constant')
-    
-    ! Read in values for temp,sal, chl,nit,sil,NH4,prt and set up c matrix
-
-    call getpardv("salinity", 7, split)
-    c(1:7,1) = split(1:7)
-    call getpardv("temperature", 7, split)
-    c(1:7,2) = split(1:7)
-    call getpardv("Phytoplankton", 7, split)
-    c(1:7,3) = split(1:7)
-    call getpardv("Nitrate", 7, split)
-    c(1:7,4) = split(1:7)
-    call getpardv("Silicon", 7, split)
-    c(1:7,5) = split(1:7)
-    call getpardv("Ammonium", 7, split)
-    c(1:7,6) = split(1:7)
-    call getpardv("Ratio", 7, split)
-    c(1:7,7) = split(1:7)
+    ! Read in values for sal, temp, chl, nit, sil, NH4, prt and set up
+    ! c matrix
+    call getpardv("salinity", 7, c(:,1))
+    call getpardv("temperature", 7, c(:,2))
+    call getpardv("Phytoplankton", 7, c(:,3))
+    call getpardv("Nitrate", 7, c(:,4))
+    call getpardv("Silicon", 7, c(:,5))
+    call getpardv("Ammonium", 7, c(:,6))
+    call getpardv("Ratio", 7, c(:,7))
   end subroutine init_fitbottom
 
   function bottom_value (arg, qty) result(value)
@@ -91,16 +78,12 @@ contains
     ! Local variable:
     integer:: index
     
-
-    
-    ! Determine if the temperature of the water coming up from the bottom boundary is 
-    ! constant or not.
-    
+    ! Apply constant temperature bottom boundary condition, if requested
     if(temp_constant) then
        c(2,6) = 0.d0
     endif
-
-
+    ! Set the coefficient matrix 2nd index for the quantity that we've
+    ! calculating
     if (qty == quantity(1)) then
        index = 1
     elseif (qty == quantity(2)) then
@@ -120,13 +103,16 @@ contains
             'Unexpected quantity: ', qty
        call exit(1)
     endif
-
-    value = c(1,index) + c(2,index)*cos(arg) + c(3,index)*sin(arg+c(4,index)) &
-         + c(5,index)*cos(3.*arg) + c(6,index)*sin(3.*arg) + c(7,index)*((arg*365.25) /( 2 * pi)) 
+    ! Calculate the bottom boundary condition value
+    value = c(1,index) &
+         + c(2,index) * cos(arg) + c(3,index) * sin(arg + c(4,index)) &
+         + c(5,index) * cos(3.0d0 * arg) &
+         + c(6,index) * sin(3.0d0 * arg) &
+         + c(7,index) * ((arg * 365.25d0) / (2.0d0 * pi)) 
   end function bottom_value
 
 
-  subroutine bot_bound_time (year,day, day_time, &
+  subroutine bot_bound_time (year, day, day_time, &
        Tbot, Sbot, Nobot, Sibot, Nhbot, Pmbot, Pnbot, Ppbot, uZbot)
     ! Calculate the values at the bottom of the grid for those
     ! quantities that we have data for from an annual fit.
@@ -136,21 +122,17 @@ contains
     use forcing, only: vary_forcing, vary
     implicit none
     ! Arguments:
-    integer, intent(in) :: year,day
+    integer, intent(in) :: year, day
     real(kind=dp), intent(in) :: day_time
     real(kind=dp), intent(out) :: Tbot, Sbot, Nobot, Sibot, Nhbot, Pmbot, &
          Pnbot, Ppbot, uZbot
     ! Local variables:
     real(kind=dp) :: arg, chl, ratio, yeartime
 
-    yeartime = (year-2002) + day/365.25 + day_time/86400/365.25
-
-    arg = 2 * pi * yeartime
-
-  
-
+    yeartime = (year - 2002) + day / 365.25d0 + day_time / 86400.0d0 / 365.25d0
+    arg = 2.0d0 * pi * yeartime
     Tbot = bottom_value(arg, 'tmp')          ! in Celcius
-    if (vary%temperature%enabled .and. .not.vary%temperature%fixed) then
+    if (vary%temperature%enabled .and. .not. vary%temperature%fixed) then
        Tbot = CtoK(Tbot) + vary%temperature%addition
     else
        Tbot = CtoK(Tbot)
@@ -159,16 +141,14 @@ contains
     Nobot = bottom_value(arg, 'nit')
     Sibot = bottom_value(arg, 'sil')
     Nhbot = bottom_value(arg, 'NH4')
-    
     chl = bottom_value(arg, 'chl')
     ratio = bottom_value(arg, 'prt')
-    
     Pmbot = chl / (ratio + 1)
     Pnbot = ratio * Pmbot
     Ppbot = Pnbot
     uZbot = Pnbot
   end subroutine bot_bound_time
-  
+
 
   subroutine bot_bound_uniform (M, D_DON, D_PON, D_refr, D_bSi)
     ! Set the values at the bottom of the grid for those
