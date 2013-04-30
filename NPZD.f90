fc83be7aea9b50bdc0690576e6663e6e053087fa
Doug Latornell <Doug.Latornell@nordion.com>
2013-04-30 14:38:34 -0700

    Fix typo and whitespace.

--- NPZD.f90
+++ NPZD.f90
@@ -223,12 +223,10 @@ contains
     call getpardv('Mesozoo, summer peak wid',3, rate_mesozoo%sumpeakwid)
     ! max igestion rate
     rate_mesozoo%R = getpard('Mesozoo, max ingestion')
-
     ! natural mortality
-    rate_mesozoo%Rm = getpard("Mesoozoo, nat mort")
+    rate_mesozoo%Rm = getpard("Mesozoo, nat mort")
     ! excretion
     rate_mesozoo%excr = getpard("Mesozoo, excretion")
-
     ! limit from predation
     rate_mesozoo%PredSlope = getpard('Mesozoo, pred slope')
     ! half saturation
